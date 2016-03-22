2d34c8e1abf160281ba287da45350dde19670c91
uppfinnarn <uppfinnarn@gmail.com>
2016-03-22 13:32:53 +0100

    Sample main.go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+)
+
+func main() ***REMOVED***
+	log.WithFields(log.Fields***REMOVED***
+		"thing": "aaaa",
+	***REMOVED***).Info("Is this working??")
+***REMOVED***
