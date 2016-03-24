b4099dabf12238c4660c55e7af86f21c2d7eb89b
uppfinnarn <uppfinnarn@gmail.com>
2016-03-24 11:28:54 +0100

    Updated tests

--- message/message_test.go
+++ message/message_test.go
@@ -8,7 +8,9 @@ func TestEncodeDecode(t *testing.T) ***REMOVED***
 	msg1 := Message***REMOVED***
 		Topic: "test",
 		Type:  "test",
-		Body:  "Abc123",
+		Fields: Fields***REMOVED***
+			"key": "Abc123",
+		***REMOVED***,
 	***REMOVED***
 	data, err := msg1.Encode()
 	msg2 := &Message***REMOVED******REMOVED***
@@ -23,7 +25,7 @@ func TestEncodeDecode(t *testing.T) ***REMOVED***
 	if msg2.Type != msg1.Type ***REMOVED***
 		t.Errorf("Type mismatch: %s != %s", msg2.Type, msg1.Type)
 	***REMOVED***
-	if msg2.Body != msg2.Body ***REMOVED***
-		t.Errorf("Body mismatch: \"%s\" != \"%s\"", msg2.Body, msg1.Body)
+	if msg2.Fields["key"] != msg2.Fields["key"] ***REMOVED***
+		t.Errorf("Fields mismatch: \"%s\" != \"%s\"", msg2.Fields, msg1.Fields)
 	***REMOVED***
 ***REMOVED***
