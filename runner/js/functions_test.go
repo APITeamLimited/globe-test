3566265e458978d627dad3470ec74d1fa2e05fe7
uppfinnarn <uppfinnarn@gmail.com>
2016-03-24 11:49:44 +0100

    Tests for JS logging

--- runner/js/functions_test.go
+++ runner/js/functions_test.go
@@ -48,3 +48,49 @@ func TestJSSleepFraction(t *testing.T) ***REMOVED***
 		t.Error("Wrong amount of sleep", d)
 	***REMOVED***
 ***REMOVED***
+
+func makeLogTestFunc(out *string) func(string) ***REMOVED***
+	return func(text string) ***REMOVED*** *out = text ***REMOVED***
+***REMOVED***
+
+func TestJSLog(t *testing.T) ***REMOVED***
+	vm := otto.New()
+	out := ""
+	vm.Set("log", jsLogFactory(makeLogTestFunc(&out)))
+	_, err := vm.Run(`log("test")`)
+	if err != nil ***REMOVED***
+		t.Error("JS Error", err)
+	***REMOVED***
+
+	if out != "test" ***REMOVED***
+		t.Errorf("Wrong output; '%s' != 'test'", out)
+	***REMOVED***
+***REMOVED***
+
+func TestJSLogInteger(t *testing.T) ***REMOVED***
+	vm := otto.New()
+	out := ""
+	vm.Set("log", jsLogFactory(makeLogTestFunc(&out)))
+	_, err := vm.Run(`log(1234)`)
+	if err != nil ***REMOVED***
+		t.Error("JS Error", err)
+	***REMOVED***
+
+	if out != "1234" ***REMOVED***
+		t.Errorf("Wrong output; '%s' != '1234'", out)
+	***REMOVED***
+***REMOVED***
+
+func TestJSLogFloat(t *testing.T) ***REMOVED***
+	vm := otto.New()
+	out := ""
+	vm.Set("log", jsLogFactory(makeLogTestFunc(&out)))
+	_, err := vm.Run(`log(12.34)`)
+	if err != nil ***REMOVED***
+		t.Error("JS Error", err)
+	***REMOVED***
+
+	if out != "12.34" ***REMOVED***
+		t.Errorf("Wrong output; '%s' != '12.34'", out)
+	***REMOVED***
+***REMOVED***
