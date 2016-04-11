d36517926f2de47ef6fd5580eb7fdd8394ca251d
uppfinnarn <uppfinnarn@gmail.com>
2016-04-11 19:29:08 +0200

    Removed all the networking and subcommand stuff

--- actions/ping/messages.go
+++
@@ -1,9 +0,0 @@
-package ping
-
-import (
-	"time"
-)
-
-type PingMessage struct ***REMOVED***
-	Time time.Time
-***REMOVED***
--- main.go
+++ main.go
@@ -140,27 +140,3 @@ func main() ***REMOVED***
 	app.Action = action
 	app.Run(os.Args)
 ***REMOVED***
-
-// func main() ***REMOVED***
-// 	// Free up -v and -h for our own flags
-// 	cli.VersionFlag.Name = "version"
-// 	cli.HelpFlag.Name = "help, ?"
-
-// 	// Bootstrap using action-registered commandline flags
-// 	app := cli.NewApp()
-// 	app.Name = "speedboat"
-// 	app.Usage = "A next-generation load generator"
-// 	app.Version = "0.0.1a1"
-// 	app.Flags = []cli.Flag***REMOVED***
-// 		cli.BoolFlag***REMOVED***
-// 			Name:  "verbose, v",
-// 			Usage: "More verbose output",
-// 		***REMOVED***,
-// 	***REMOVED***
-// 	app.Commands = client.GlobalCommands
-// 	app.Before = func(c *cli.Context) error ***REMOVED***
-// 		configureLogging(c)
-// 		return nil
-// 	***REMOVED***
-// 	app.Run(os.Args)
-// ***REMOVED***
