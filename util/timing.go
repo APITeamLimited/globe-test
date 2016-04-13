08af32b2f1b241d22534824182c9f7a2e4957a5f
uppfinnarn <uppfinnarn@gmail.com>
2016-04-13 13:01:05 +0200

    How's this

--- util/timing.go
+++ util/timing.go
@@ -9,19 +9,15 @@ func Time(fn func()) time.Duration ***REMOVED***
 	m := runtime.MemStats***REMOVED******REMOVED***
 	runtime.ReadMemStats(&m)
 
-	numGC1 := m.NumGC
+	gcTotal1 := m.PauseTotalNs
 
 	startTime := time.Now()
 	fn()
 	duration := time.Since(startTime)
 
 	runtime.ReadMemStats(&m)
-	numGC2 := m.NumGC
+	gcTotal2 := m.PauseTotalNs
 
-	gcTotal := uint64(0)
-	for i := numGC1; i < numGC2; i++ ***REMOVED***
-		gcTotal += m.PauseNs[(i+255)%256]
-	***REMOVED***
-
-	return duration - time.Duration(gcTotal)
+	gcTotal := time.Duration(gcTotal2 - gcTotal1)
+	return duration - gcTotal
 ***REMOVED***
