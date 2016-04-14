028519db2b157ee4f731d3553eb16d3ea3e34c71
uppfinnarn <uppfinnarn@gmail.com>
2016-04-14 11:54:44 +0200

    ...

--- runner/simple/simple.go
+++ runner/simple/simple.go
@@ -62,7 +62,7 @@ func (r *SimpleRunner) Run(ctx context.Context) <-chan runner.Result ***REMOVED*
 			case res := <-results:
 				ch <- res
 			case <-ctx.Done():
-				close(cancelRequest)
+				// close(cancelRequest)
 				return
 			***REMOVED***
 		***REMOVED***
