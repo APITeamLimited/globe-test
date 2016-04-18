03f73e8bca6b91b1df46ff9b152e186f113a2b73
uppfinnarn <uppfinnarn@gmail.com>
2016-04-18 16:57:52 +0200

    I can run simple Lua scripts

--- runner/lua/lua.go
+++ runner/lua/lua.go
@@ -2,16 +2,19 @@ package lua
 
 import (
 	"github.com/loadimpact/speedboat/runner"
+	"github.com/yuin/gopher-lua"
 	"golang.org/x/net/context"
-	// "github.com/Shopify/go-lua"
 )
 
 type LuaRunner struct ***REMOVED***
-	Script string
+	Filename, Source string
 ***REMOVED***
 
 func New(filename, src string) *LuaRunner ***REMOVED***
-	return &LuaRunner***REMOVED******REMOVED***
+	return &LuaRunner***REMOVED***
+		Filename: filename,
+		Source:   src,
+	***REMOVED***
 ***REMOVED***
 
 func (r *LuaRunner) Run(ctx context.Context) <-chan runner.Result ***REMOVED***
@@ -19,6 +22,29 @@ func (r *LuaRunner) Run(ctx context.Context) <-chan runner.Result ***REMOVED***
 
 	go func() ***REMOVED***
 		defer close(ch)
+
+		L := lua.NewState()
+		defer L.Close()
+
+		// Try to load the script, abort execution if it fails
+		lfn, err := L.LoadString(r.Source)
+		if err != nil ***REMOVED***
+			ch <- runner.Result***REMOVED***Error: err***REMOVED***
+			return
+		***REMOVED***
+
+		for ***REMOVED***
+			L.Push(lfn)
+			if err := L.PCall(0, 0, nil); err != nil ***REMOVED***
+				ch <- runner.Result***REMOVED***Error: err***REMOVED***
+			***REMOVED***
+
+			select ***REMOVED***
+			case <-ctx.Done():
+				return
+			default:
+			***REMOVED***
+		***REMOVED***
 	***REMOVED***()
 
 	return ch
