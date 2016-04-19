873eda06f60fc9a27742d6b6d63b9b2b6d4b79d7
uppfinnarn <uppfinnarn@gmail.com>
2016-04-19 11:12:29 +0200

    Benchmarks for running Lua

---
+++ runner/lua/lua_test.go
@@ -0,0 +1,16 @@
+package lua
+
+import (
+	"golang.org/x/net/context"
+	"testing"
+)
+
+func BenchmarkRunEmpty(b *testing.B) ***REMOVED***
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	r := New("script.lua", "")
+	for i := 0; i < b.N; i++ ***REMOVED***
+		r.Run(ctx)
+	***REMOVED***
+***REMOVED***
