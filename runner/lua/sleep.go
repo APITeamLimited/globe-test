75f757637e8ba2e06f10af5a07b3c38502b2b8d5
uppfinnarn <uppfinnarn@gmail.com>
2016-04-19 11:42:51 +0200

    Float sleeps

--- runner/lua/sleep.go
+++ runner/lua/sleep.go
@@ -6,7 +6,7 @@ import (
 )
 
 func (vu *VUContext) Sleep(L *lua.LState) int ***REMOVED***
-	t := L.ToInt(1)
+	t := L.ToNumber(1)
 	time.Sleep(time.Duration(t) * time.Second)
 	return 0
 ***REMOVED***
