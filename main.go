959b914e7743bd422ab71e38a87ffd1e94d15fe9
uppfinnarn <uppfinnarn@gmail.com>
2016-04-25 16:07:30 +0200

    We're going with V8

--- main.go
+++ main.go
@@ -7,10 +7,6 @@ import (
 	"github.com/loadimpact/speedboat/loadtest"
 	"github.com/loadimpact/speedboat/report"
 	"github.com/loadimpact/speedboat/runner"
-	"github.com/loadimpact/speedboat/runner/ank"
-	"github.com/loadimpact/speedboat/runner/duktapejs"
-	"github.com/loadimpact/speedboat/runner/lua"
-	"github.com/loadimpact/speedboat/runner/ottojs"
 	"github.com/loadimpact/speedboat/runner/simple"
 	"github.com/loadimpact/speedboat/runner/v8js"
 	"golang.org/x/net/context"
@@ -101,23 +97,8 @@ func action(c *cli.Context) ***REMOVED***
 	if test.Script != "" ***REMOVED***
 		ext := path.Ext(test.Script)
 		switch ext ***REMOVED***
-		case ".lua":
-			r = lua.New(test.Script, test.Source)
 		case ".js":
-			switch c.String("impl") ***REMOVED***
-			case "otto":
-				r = ottojs.New(test.Script, test.Source)
-			case "duk":
-				r = duktapejs.New(test.Script, test.Source)
-			case "v8":
-				r = v8js.New(test.Script, test.Source)
-			case "":
-				log.Fatal("No implementation specified; use --impl ***REMOVED***otto,duk***REMOVED***")
-			default:
-				log.Fatal("Unknown implementation")
-			***REMOVED***
-		case ".ank":
-			r = ank.New(test.Script, test.Source)
+			r = v8js.New(test.Script, test.Source)
 		default:
 			log.WithField("ext", ext).Fatal("No runner found")
 		***REMOVED***
@@ -236,10 +217,6 @@ func main() ***REMOVED***
 		cli.StringFlag***REMOVED***
 			Name:  "out-file, o",
 			Usage: "Output raw metrics to a file",
-		***REMOVED***,
-		cli.StringFlag***REMOVED***
-			Name:  "impl, i",
-			Usage: "Specify a runner implementation",
 		***REMOVED***,
 	***REMOVED***
 	app.Before = func(c *cli.Context) error ***REMOVED***
--- runner/ank/sleep.go
+++
@@ -1,9 +0,0 @@
-package ank
-
-import (
-	"time"
-)
-
-func (vu *VUContext) Sleep(t float64) ***REMOVED***
-	time.Sleep(time.Duration(t) * time.Second)
-***REMOVED***
