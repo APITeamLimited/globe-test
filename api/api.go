f2da7c7ba3de5c16eb946876f711fa421b4ca085
uppfinnarn <uppfinnarn@gmail.com>
2016-05-03 14:00:32 +0200

    Migrated console

--- api/api.go
+++ api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"github.com/loadimpact/speedboat/api/console"
 	"github.com/loadimpact/speedboat/api/global"
 	"github.com/loadimpact/speedboat/api/http"
 )
@@ -8,8 +9,9 @@ import (
 type RegisterFunc func() map[string]interface***REMOVED******REMOVED***
 
 var API = map[string]RegisterFunc***REMOVED***
-	"global": global.New,
-	"http":   http.New,
+	"global":  global.New,
+	"console": console.New,
+	"http":    http.New,
 ***REMOVED***
 
 func New() map[string]map[string]interface***REMOVED******REMOVED*** ***REMOVED***
--- api/console/console.go
+++ api/console/console.go
@@ -1,9 +1,19 @@
-package v8js
+package console
 
 import (
 	log "github.com/Sirupsen/logrus"
 	"strconv"
 )
+
+var members = map[string]interface***REMOVED******REMOVED******REMOVED***
+	"log":   Log,
+	"warn":  Warn,
+	"error": Error,
+***REMOVED***
+
+func New() map[string]interface***REMOVED******REMOVED*** ***REMOVED***
+	return members
+***REMOVED***
 
 func consoleLogFields(args []interface***REMOVED******REMOVED***) log.Fields ***REMOVED***
 	fields := log.Fields***REMOVED******REMOVED***
@@ -14,14 +24,14 @@ func consoleLogFields(args []interface***REMOVED******REMOVED***) log.Fields ***
 ***REMOVED***
 
 // TODO: Match console.log()'s sprintf()-like formatting behavior
-func (vu *VUContext) ConsoleLog(msg string, args ...interface***REMOVED******REMOVED***) ***REMOVED***
+func Log(msg string, args ...interface***REMOVED******REMOVED***) ***REMOVED***
 	log.WithFields(consoleLogFields(args)).Info(msg)
 ***REMOVED***
 
-func (vu *VUContext) ConsoleWarn(msg string, args ...interface***REMOVED******REMOVED***) ***REMOVED***
+func Warn(msg string, args ...interface***REMOVED******REMOVED***) ***REMOVED***
 	log.WithFields(consoleLogFields(args)).Warn(msg)
 ***REMOVED***
 
-func (vu *VUContext) ConsoleError(msg string, args ...interface***REMOVED******REMOVED***) ***REMOVED***
+func Error(msg string, args ...interface***REMOVED******REMOVED***) ***REMOVED***
 	log.WithFields(consoleLogFields(args)).Error(msg)
 ***REMOVED***
