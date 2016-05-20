5077d78c6bc1f163dbf881da4f1371e708db85f8
uppfinnarn <uppfinnarn@gmail.com>
2016-05-20 17:58:32 +0200

    Much better (it actually runs scripts!)

--- runner/js/js.go
+++ runner/js/js.go
@@ -1,6 +1,7 @@
 package js
 
 import (
+	"errors"
 	"github.com/loadimpact/speedboat/loadtest"
 	"github.com/loadimpact/speedboat/runner"
 	"golang.org/x/net/context"
@@ -22,36 +23,54 @@ func (r *Runner) Run(ctx context.Context, t loadtest.LoadTest, id int64) <-chan 
 		defer close(ch)
 
 		c := duktape.New()
-		setupInternals(c, t, id, ch)
+		defer c.Destroy()
+
+		c.PushGlobalObject()
+		c.PushObject() // __internal__
+
+		pushModules(c, t, id, ch)
+		c.PutPropString(-2, "modules")
+
+		c.PutPropString(-2, "__internal__")
+
+		if top := c.GetTopIndex(); top != 0 ***REMOVED***
+			panic("PROGRAMMING ERROR: Excess items on stack: " + strconv.Itoa(top+1))
+		***REMOVED***
+
+		if err := c.PcompileString(0, t.Source); err != nil ***REMOVED***
+			ch <- runner.Result***REMOVED***Error: err***REMOVED***
+			return
+		***REMOVED***
+
+		for ***REMOVED***
+			select ***REMOVED***
+			case <-ctx.Done():
+				return
+			default:
+				c.DupTop()
+				if c.Pcall(0) != duktape.ErrNone ***REMOVED***
+					err := errors.New(c.SafeToString(-1))
+					ch <- runner.Result***REMOVED***Error: err***REMOVED***
+				***REMOVED***
+				c.Pop() // Pop return value
+			***REMOVED***
+		***REMOVED***
 	***REMOVED***()
 
 	return ch
 ***REMOVED***
 
-func setupInternals(c *duktape.Context, t loadtest.LoadTest, id int64, ch <-chan runner.Result) ***REMOVED***
+func pushModules(c *duktape.Context, t loadtest.LoadTest, id int64, ch <-chan runner.Result) ***REMOVED***
 	api := map[string]map[string]apiFunc***REMOVED***
 		"http": map[string]apiFunc***REMOVED***
 			"get": apiHTTPGet,
 		***REMOVED***,
 	***REMOVED***
 
-	c.PushGlobalObject()
-
-	c.PushObject() // __internal__
-
 	c.PushObject() // __internal__.modules
 	for name, mod := range api ***REMOVED***
 		pushModule(c, ch, mod)
 		c.PutPropString(-2, name)
-	***REMOVED***
-	c.PutPropString(-2, "modules")
-
-	c.PutPropString(-2, "__internal__")
-
-	c.Pop() // global object
-
-	if top := c.GetTopIndex(); !(top < 0) ***REMOVED*** // < 0 = invalid index = empty stack
-		panic("PROGRAMMING ERROR: Stack depth must be 0, is: " + strconv.Itoa(top+1))
 	***REMOVED***
 ***REMOVED***
 
