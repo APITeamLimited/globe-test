69bc7148fe264f66cddd52ce02cc8ed83f690620
uppfinnarn <uppfinnarn@gmail.com>
2016-05-20 16:43:18 +0200

    Better structure

--- runner/js/js.go
+++ runner/js/js.go
@@ -21,34 +21,38 @@ func (r *Runner) Run(ctx context.Context, t loadtest.LoadTest, id int64) <-chan 
 	go func() ***REMOVED***
 		defer close(ch)
 
-		api := map[string]map[string]apiFunc***REMOVED***
-			"http": map[string]apiFunc***REMOVED***
-				"get": apiHTTPGet,
-			***REMOVED***,
-		***REMOVED***
-
 		c := duktape.New()
-		c.PushGlobalObject()
-
-		c.PushObject() // __internal__
-
-		c.PushObject() // __internal__.modules
-		for name, mod := range api ***REMOVED***
-			pushModule(c, ch, mod)
-			c.PutPropString(-2, name)
-		***REMOVED***
-		c.PutPropString(-2, "modules")
-
-		c.PutPropString(-2, "__internal__")
-
-		c.Pop() // global object
-
-		if top := c.GetTopIndex(); !(top < 0) ***REMOVED*** // < 0 = invalid index = empty stack
-			panic("PROGRAMMING ERROR: Stack depth must be 0, is: " + strconv.Itoa(top+1))
-		***REMOVED***
+		setupInternals(c, t, id, ch)
 	***REMOVED***()
 
 	return ch
+***REMOVED***
+
+func setupInternals(c *duktape.Context, t loadtest.LoadTest, id int64, ch <-chan runner.Result) ***REMOVED***
+	api := map[string]map[string]apiFunc***REMOVED***
+		"http": map[string]apiFunc***REMOVED***
+			"get": apiHTTPGet,
+		***REMOVED***,
+	***REMOVED***
+
+	c.PushGlobalObject()
+
+	c.PushObject() // __internal__
+
+	c.PushObject() // __internal__.modules
+	for name, mod := range api ***REMOVED***
+		pushModule(c, ch, mod)
+		c.PutPropString(-2, name)
+	***REMOVED***
+	c.PutPropString(-2, "modules")
+
+	c.PutPropString(-2, "__internal__")
+
+	c.Pop() // global object
+
+	if top := c.GetTopIndex(); !(top < 0) ***REMOVED*** // < 0 = invalid index = empty stack
+		panic("PROGRAMMING ERROR: Stack depth must be 0, is: " + strconv.Itoa(top+1))
+	***REMOVED***
 ***REMOVED***
 
 func pushModule(c *duktape.Context, ch <-chan runner.Result, members map[string]apiFunc) int ***REMOVED***
