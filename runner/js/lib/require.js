1d143c7346fae8551b1f2356df1d87e762eb821b
uppfinnarn <uppfinnarn@gmail.com>
2016-05-25 12:24:10 +0200

    Oops

--- runner/js/lib/require.js
+++ runner/js/lib/require.js
@@ -1,10 +1,9 @@
 "use strict";
 
 function require(name) ***REMOVED***
-	print(name);
-	// var mod = __internal__.modules[name];
-	// if (!mod) ***REMOVED***
-	// 	throw new Error("Unknown module: " + name);
-	// ***REMOVED***
-	// return mod;
+	var mod = __internal__.modules[name];
+	if (!mod) ***REMOVED***
+		throw new Error("Unknown module: " + name);
+	***REMOVED***
+	return mod;
 ***REMOVED***
