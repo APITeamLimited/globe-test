b9dbc61d745c8cc3a921b39c44f52ad20f6c0aa5
uppfinnarn <uppfinnarn@gmail.com>
2016-05-30 17:44:06 +0200

    [test] Fixed JS engine tests

--- js/api_test.go
+++ js/api_test.go
@@ -1,33 +1,30 @@
 package js
 
 import (
-	"github.com/loadimpact/speedboat/loadtest"
-	"github.com/loadimpact/speedboat/runner"
+	"github.com/loadimpact/speedboat"
 	"gopkg.in/olebedev/go-duktape.v2"
 	"testing"
 	"time"
 )
 
-func testJSContext() (*Runner, *duktape.Context, chan runner.Result) ***REMOVED***
-	ch := make(chan runner.Result, 100)
-	r := New()
-	c, err := r.newJSContext(loadtest.LoadTest***REMOVED***
+func testJSContext() (*Runner, *duktape.Context) ***REMOVED***
+	r := New("")
+	c, err := r.newJSContext(speedboat.Test***REMOVED***
 		URL:    "http://example.com",
 		Script: "script.js",
-		Source: "~ not actually valid JS ~",
-		Stages: []loadtest.Stage***REMOVED***
-			loadtest.Stage***REMOVED***VUs: loadtest.VUSpec***REMOVED***Start: 10, End: 100***REMOVED***, Duration: 10 * time.Second***REMOVED***,
+		Stages: []speedboat.TestStage***REMOVED***
+			speedboat.TestStage***REMOVED***StartVUs: 10, EndVUs: 100, Duration: 10 * time.Second***REMOVED***,
 		***REMOVED***,
-	***REMOVED***, 1, ch)
+	***REMOVED***, 1)
 	if err != nil ***REMOVED***
 		panic(err)
 	***REMOVED***
-	return r, c, ch
+	return r, c
 ***REMOVED***
 
 func TestHTTPSetMaxConnectionsPerHost(t *testing.T) ***REMOVED***
 	src := `require('http').setMaxConnectionsPerHost(200);`
-	r, c, _ := testJSContext()
+	r, c := testJSContext()
 	if err := c.PevalString(src); err != nil ***REMOVED***
 		t.Fatalf("Couldn't run script: %s", err)
 	***REMOVED***
@@ -38,20 +35,12 @@ func TestHTTPSetMaxConnectionsPerHost(t *testing.T) ***REMOVED***
 
 func TestHTTPSetMaxConnectionsPerHostNegative(t *testing.T) ***REMOVED***
 	src := `require('http').setMaxConnectionsPerHost(-1);`
-	r, c, ch := testJSContext()
+	r, c := testJSContext()
 	before := r.Client.MaxConnsPerHost
 	if err := c.PevalString(src); err != nil ***REMOVED***
 		t.Fatalf("Couldn't run script: %s", err)
 	***REMOVED***
-	select ***REMOVED***
-	case res := <-ch:
-		if res.Error == nil ***REMOVED***
-			t.Error("No error reported!")
-		***REMOVED***
-		if r.Client.MaxConnsPerHost != before ***REMOVED***
-			t.Errorf("Max connections changed! %d", r.Client.MaxConnsPerHost)
-		***REMOVED***
-	default:
-		t.Error("No results")
+	if r.Client.MaxConnsPerHost != before ***REMOVED***
+		t.Errorf("Max connections changed! %d", r.Client.MaxConnsPerHost)
 	***REMOVED***
 ***REMOVED***
