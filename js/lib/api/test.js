00670f69c955489acb8a07e77a2ef29422e3d068
uppfinnarn <uppfinnarn@gmail.com>
2016-06-03 15:32:34 +0200

    [fix] Make sure modules exist before extending

--- js/lib/api/test.js
+++ js/lib/api/test.js
@@ -1,5 +1,7 @@
 "use strict";
 
+__modules__.test = __modules__.test || ***REMOVED******REMOVED***;
+
 __modules__.test.url = function() ***REMOVED***
 	return __internal__.data.test.url;
 ***REMOVED***
--- js/lib/api/vu.js
+++ js/lib/api/vu.js
@@ -1,5 +1,7 @@
 "use strict";
 
+__modules__.vu = __modules__.vu || ***REMOVED******REMOVED***;
+
 __modules__.vu.id = function() ***REMOVED***
 	return __internal__.data.id;
 ***REMOVED***
