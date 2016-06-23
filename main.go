eac305b331f5db5d726e076661de61108bade35f
Ragnar Lonn <ragnar@loadimpact.com>
2016-06-23 15:17:41 +0200

    [fix] Made help template text a const at top of file

--- main.go
+++ main.go
@@ -25,6 +25,22 @@ const (
 	typeJS  = "js"
 )
 
+// Help text template
+const (
+	helpTemplate = `NAME:
+   ***REMOVED******REMOVED***.Name***REMOVED******REMOVED*** - ***REMOVED******REMOVED***.Usage***REMOVED******REMOVED***
+
+USAGE:
+   ***REMOVED******REMOVED***if .UsageText***REMOVED******REMOVED******REMOVED******REMOVED***.UsageText***REMOVED******REMOVED******REMOVED******REMOVED***else***REMOVED******REMOVED******REMOVED******REMOVED***.HelpName***REMOVED******REMOVED*** ***REMOVED******REMOVED***if .VisibleFlags***REMOVED******REMOVED***[options] ***REMOVED******REMOVED***end***REMOVED******REMOVED***filename|url***REMOVED******REMOVED***end***REMOVED******REMOVED***
+   ***REMOVED******REMOVED***if .Version***REMOVED******REMOVED******REMOVED******REMOVED***if not .HideVersion***REMOVED******REMOVED***
+VERSION:
+   ***REMOVED******REMOVED***.Version***REMOVED******REMOVED***
+   ***REMOVED******REMOVED***end***REMOVED******REMOVED******REMOVED******REMOVED***end***REMOVED******REMOVED******REMOVED******REMOVED***if .VisibleFlags***REMOVED******REMOVED***
+OPTIONS:
+   ***REMOVED******REMOVED***range .VisibleFlags***REMOVED******REMOVED******REMOVED******REMOVED***.***REMOVED******REMOVED***
+   ***REMOVED******REMOVED***end***REMOVED******REMOVED******REMOVED******REMOVED***end***REMOVED******REMOVED***`
+)
+
 // Configure the global logger.
 func configureLogging(c *cli.Context) ***REMOVED***
 	log.SetLevel(log.InfoLevel)
@@ -276,19 +292,7 @@ func main() ***REMOVED***
 	// Free up -v and -h for our own flags
 	cli.VersionFlag.Name = "version"
 	cli.HelpFlag.Name = "help, ?"
-	cli.AppHelpTemplate = `NAME:
-   ***REMOVED******REMOVED***.Name***REMOVED******REMOVED*** - ***REMOVED******REMOVED***.Usage***REMOVED******REMOVED***
-
-USAGE:
-   ***REMOVED******REMOVED***if .UsageText***REMOVED******REMOVED******REMOVED******REMOVED***.UsageText***REMOVED******REMOVED******REMOVED******REMOVED***else***REMOVED******REMOVED******REMOVED******REMOVED***.HelpName***REMOVED******REMOVED*** ***REMOVED******REMOVED***if .VisibleFlags***REMOVED******REMOVED***[options] ***REMOVED******REMOVED***end***REMOVED******REMOVED***filename|url***REMOVED******REMOVED***end***REMOVED******REMOVED***
-   ***REMOVED******REMOVED***if .Version***REMOVED******REMOVED******REMOVED******REMOVED***if not .HideVersion***REMOVED******REMOVED***
-VERSION:
-   ***REMOVED******REMOVED***.Version***REMOVED******REMOVED***
-   ***REMOVED******REMOVED***end***REMOVED******REMOVED******REMOVED******REMOVED***end***REMOVED******REMOVED******REMOVED******REMOVED***if .VisibleFlags***REMOVED******REMOVED***
-OPTIONS:
-   ***REMOVED******REMOVED***range .VisibleFlags***REMOVED******REMOVED******REMOVED******REMOVED***.***REMOVED******REMOVED***
-   ***REMOVED******REMOVED***end***REMOVED******REMOVED******REMOVED******REMOVED***end***REMOVED******REMOVED***`
-
+	cli.AppHelpTemplate = helpTemplate
 	// Bootstrap using action-registered commandline flags
 	app := cli.NewApp()
 	app.Commands = nil
