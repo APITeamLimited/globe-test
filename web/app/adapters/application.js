bcaf1409e928ff157d15686a08cff92c42ef6a19
uppfinnarn <uppfinnarn@gmail.com>
2016-09-07 19:20:56 +0200

    [refactor] This should be done in an inflector

--- web/app/adapters/application.js
+++ web/app/adapters/application.js
@@ -2,14 +2,6 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend(***REMOVED***
   namespace: "v1",
-  pathForType(modelName) ***REMOVED***
-    switch (modelName) ***REMOVED***
-    case 'status':
-      return modelName
-    default:
-      return this._super(modelName);
-    ***REMOVED***
-  ***REMOVED***,
   urlForFindRecord(id, modelName, snapshot) ***REMOVED***
     if (id === "default") ***REMOVED***
       return this.urlForFindAll(modelName, snapshot);
---
+++ web/tests/unit/initializers/inflector-test.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+import InflectorInitializer from 'speedboat/initializers/inflector';
+import ***REMOVED*** module, test ***REMOVED*** from 'qunit';
+
+let application;
+
+module('Unit | Initializer | inflector', ***REMOVED***
+  beforeEach() ***REMOVED***
+    Ember.run(function() ***REMOVED***
+      application = Ember.Application.create();
+      application.deferReadiness();
+    ***REMOVED***);
+  ***REMOVED***
+***REMOVED***);
+
+// Replace this with your real tests.
+test('it works', function(assert) ***REMOVED***
+  InflectorInitializer.initialize(application);
+
+  // you would normally confirm the results of the initializer here
+  assert.ok(true);
+***REMOVED***);
