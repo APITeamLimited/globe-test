6365667fc67f1d5894719bf2dec7eb80bd7eab4b
uppfinnarn <uppfinnarn@gmail.com>
2016-09-19 15:34:17 +0200

    [feat] Refresh paused tests less frequently

--- web/app/routes/application.js
+++ web/app/routes/application.js
@@ -1,13 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend(***REMOVED***
-  _scheduleRefresh: Ember.on('init', function() ***REMOVED***
+  _scheduleRefresh: Ember.on('init', function(delay = 5000) ***REMOVED***
     Ember.run.later(()=> ***REMOVED***
       this.refresh();
-      if (this.get('controller.model.running')) ***REMOVED***
-        this._scheduleRefresh();
-      ***REMOVED***
-    ***REMOVED***, 5000);
+      this._scheduleRefresh(this.get('controller.model.running') ? 5000 : 15000);
+    ***REMOVED***, delay);
   ***REMOVED***),
   model() ***REMOVED***
     return this.get('store').findRecord('status', 'default');
