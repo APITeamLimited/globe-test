48a8473a1b3fe0daf0341a09cb539e87639acbd9
uppfinnarn <uppfinnarn@gmail.com>
2016-11-17 17:26:29 +0100

    [test] Fixed Lerp tests

--- lib/util_test.go
+++ lib/util_test.go
@@ -6,17 +6,17 @@ import (
 	"testing"
 )
 
-func TestEase(t *testing.T) ***REMOVED***
-	// x[y][t] = x1, tx = 0, ty = 100
-	data := map[int64]map[int64]map[int64]int64***REMOVED***
-		0: map[int64]map[int64]int64***REMOVED***
-			0:   map[int64]int64***REMOVED***0: 0, 10: 0, 50: 0, 100: 0***REMOVED***,
-			100: map[int64]int64***REMOVED***0: 0, 10: 10, 50: 50, 100: 100***REMOVED***,
-			500: map[int64]int64***REMOVED***0: 0, 10: 50, 50: 250, 100: 500***REMOVED***,
+func TestLerp(t *testing.T) ***REMOVED***
+	// data[x][y][t] = v
+	data := map[int64]map[int64]map[float64]int64***REMOVED***
+		0: map[int64]map[float64]int64***REMOVED***
+			0:   map[float64]int64***REMOVED***0.0: 0, 0.10: 0, 0.5: 0, 1.0: 0***REMOVED***,
+			100: map[float64]int64***REMOVED***0.0: 0, 0.10: 10, 0.5: 50, 1.0: 100***REMOVED***,
+			500: map[float64]int64***REMOVED***0.0: 0, 0.10: 50, 0.5: 250, 1.0: 500***REMOVED***,
 		***REMOVED***,
-		100: map[int64]map[int64]int64***REMOVED***
-			200: map[int64]int64***REMOVED***0: 100, 10: 110, 50: 150, 100: 200***REMOVED***,
-			0:   map[int64]int64***REMOVED***0: 100, 10: 90, 50: 50, 100: 0***REMOVED***,
+		100: map[int64]map[float64]int64***REMOVED***
+			200: map[float64]int64***REMOVED***0.0: 100, 0.1: 110, 0.5: 150, 1.0: 200***REMOVED***,
+			0:   map[float64]int64***REMOVED***0.0: 100, 0.1: 90, 0.5: 50, 1.0: 0***REMOVED***,
 		***REMOVED***,
 	***REMOVED***
 
@@ -24,9 +24,9 @@ func TestEase(t *testing.T) ***REMOVED***
 		t.Run("x="+strconv.FormatInt(x, 10), func(t *testing.T) ***REMOVED***
 			for y, data := range data ***REMOVED***
 				t.Run("y="+strconv.FormatInt(y, 10), func(t *testing.T) ***REMOVED***
-					for t0, x1 := range data ***REMOVED***
-						t.Run("t="+strconv.FormatInt(t0, 10), func(t *testing.T) ***REMOVED***
-							assert.Equal(t, x1, Ease(t0, 0, 100, x, y))
+					for t_, x1 := range data ***REMOVED***
+						t.Run("t="+strconv.FormatFloat(t_, 'f', 2, 64), func(t *testing.T) ***REMOVED***
+							assert.Equal(t, x1, Lerp(x, y, t_))
 						***REMOVED***)
 					***REMOVED***
 				***REMOVED***)
