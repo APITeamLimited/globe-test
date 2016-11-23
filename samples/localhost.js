3a4dbd38f69edbeb199b6a2a274435837e225021
uppfinnarn <uppfinnarn@gmail.com>
2016-11-23 16:21:28 +0100

    [docs] Updated localhost.js with some groups

--- samples/localhost.js
+++ samples/localhost.js
@@ -1,8 +1,20 @@
-import ***REMOVED*** check ***REMOVED*** from "speedboat";
+import ***REMOVED*** group, check ***REMOVED*** from "speedboat";
 import http from "speedboat/http";
 
 export default function() ***REMOVED***
-	check(http.get("http://localhost:8080/"), ***REMOVED***
-		"status is 200": (v) => v.status === 200,
+	group("front page", function() ***REMOVED***
+		check(http.get("http://localhost:8080/"), ***REMOVED***
+			"status is 200": (res) => res.status === 200,
+		***REMOVED***);
+	***REMOVED***);
+	group("stylesheet", function() ***REMOVED***
+		check(http.get("http://localhost:8080/style.css"), ***REMOVED***
+			"status is 200": (res) => res.status === 200,
+		***REMOVED***);
+	***REMOVED***);
+	group("image", function() ***REMOVED***
+		check(http.get("http://localhost:8080/teddy.jpg"), ***REMOVED***
+			"status is 200": (res) => res.status === 200,
+		***REMOVED***);
 	***REMOVED***);
 ***REMOVED***
