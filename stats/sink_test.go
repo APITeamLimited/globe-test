38857f880dcd87df3196cc376e779f9ad1bb4c4e
uppfinnarn <uppfinnarn@gmail.com>
2016-12-14 20:54:19 +0100

    [feat] DummySink for testing purposes

---
+++ stats/sink_test.go
@@ -0,0 +1,36 @@
+/*
+ *
+ * k6 - a next-generation load testing tool
+ * Copyright (C) 2016 Load Impact
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package stats
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDummySinkAddPanics(t *testing.T) ***REMOVED***
+	assert.Panics(t, func() ***REMOVED***
+		DummySink***REMOVED******REMOVED***.Add(Sample***REMOVED******REMOVED***)
+	***REMOVED***)
+***REMOVED***
+
+func TestDummySinkFormatReturnsItself(t *testing.T) ***REMOVED***
+	assert.Equal(t, map[string]float64***REMOVED***"a": 1***REMOVED***, DummySink***REMOVED***"a": 1***REMOVED***.Format())
+***REMOVED***
