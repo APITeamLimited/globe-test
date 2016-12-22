20da6eaca9b7d130fb17458c6a042dabd71d6536
uppfinnarn <uppfinnarn@gmail.com>
2016-12-22 22:10:44 +0100

    [test/fix] Okay, fine, allow two ticks' margin

--- lib/engine_test.go
+++ lib/engine_test.go
@@ -165,7 +165,7 @@ func TestEngineRun(t *testing.T) ***REMOVED***
 		ctx, _ := context.WithTimeout(context.Background(), d)
 		startTime := time.Now()
 		assert.NoError(t, e.Run(ctx))
-		assert.WithinDuration(t, startTime.Add(d), startTime.Add(e.AtTime()), 1*TickRate)
+		assert.WithinDuration(t, startTime.Add(d), startTime.Add(e.AtTime()), 2*TickRate)
 	***REMOVED***)
 ***REMOVED***
 
