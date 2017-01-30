03a67e0e8135641653afd4ff80a71e0f91335442
Martin Fijal <martin@loadimpact.com>
2017-01-30 14:01:50 +0100

    Add basic cloud communication

---
+++ stats/cloud/client.go
@@ -0,0 +1,162 @@
+package cloud
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/loadimpact/k6/stats"
+)
+
+// Client handles communication with Load Impact cloud API.
+type Client struct ***REMOVED***
+	client  *http.Client
+	token   string
+	baseURL string
+***REMOVED***
+
+type ErrorResponse struct ***REMOVED***
+	Message string `json:"message"`
+***REMOVED***
+
+func NewClient(token string) *Client ***REMOVED***
+
+	var client = &http.Client***REMOVED***
+		Timeout: 30 * time.Second,
+	***REMOVED***
+
+	host := os.Getenv("K6CLOUD_HOST")
+	if host == "" ***REMOVED***
+		host = "http://localhost:5000"
+	***REMOVED***
+
+	baseURL := fmt.Sprintf("%s/v1", host)
+
+	c := &Client***REMOVED***
+		client:  client,
+		token:   token,
+		baseURL: baseURL,
+	***REMOVED***
+	return c
+***REMOVED***
+
+func (c *Client) NewRequest(method, url string, data interface***REMOVED******REMOVED***) (*http.Request, error) ***REMOVED***
+	var buf io.Reader
+
+	if data != nil ***REMOVED***
+		b, err := json.Marshal(&data)
+		if err != nil ***REMOVED***
+			return nil, err
+		***REMOVED***
+
+		buf = bytes.NewBuffer(b)
+	***REMOVED***
+
+	return http.NewRequest(method, url, buf)
+***REMOVED***
+
+func (c *Client) Do(req *http.Request, v interface***REMOVED******REMOVED***) error ***REMOVED***
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Token %s", c.token))
+
+	resp, err := c.client.Do(req)
+	if err != nil ***REMOVED***
+		return err
+	***REMOVED***
+	defer func() ***REMOVED***
+		err := resp.Body.Close()
+		if err != nil ***REMOVED***
+			log.Errorln(err)
+		***REMOVED***
+	***REMOVED***()
+
+	if v != nil ***REMOVED***
+		err = json.NewDecoder(resp.Body).Decode(v)
+		if err == io.EOF ***REMOVED***
+			err = nil // Ignore EOF from empty body
+		***REMOVED***
+	***REMOVED***
+
+	return err
+***REMOVED***
+
+type TestRun struct ***REMOVED***
+	Name        string              `json:"name"`
+	ProjectID   int                 `json:"project_id,omitempty"`
+	Threasholds map[string][]string `json:"thresholds"`
+***REMOVED***
+
+type CreateTestRunResponse struct ***REMOVED***
+	ReferenceID string `json:"reference_id"`
+***REMOVED***
+
+func (c *Client) CreateTestRun(name string, thresholds map[string][]string) *CreateTestRunResponse ***REMOVED***
+	testRun := TestRun***REMOVED***Name: name, Threasholds: thresholds***REMOVED***
+
+	url := fmt.Sprintf("%s/tests", c.baseURL)
+	req, err := c.NewRequest("POST", url, testRun)
+	if err != nil ***REMOVED***
+		return nil
+	***REMOVED***
+
+	var ctrr = CreateTestRunResponse***REMOVED******REMOVED***
+	err = c.Do(req, &ctrr)
+	if err != nil ***REMOVED***
+		return nil
+	***REMOVED***
+
+	return &ctrr
+***REMOVED***
+
+func (c *Client) PushMetric(referenceID string, samples []*Sample) ***REMOVED***
+	url := fmt.Sprintf("%s/metrics/%s", c.baseURL, referenceID)
+
+	req, err := c.NewRequest("POST", url, samples)
+	if err != nil ***REMOVED***
+		return
+	***REMOVED***
+
+	err = c.Do(req, nil)
+	if err != nil ***REMOVED***
+		return
+	***REMOVED***
+***REMOVED***
+
+func (c *Client) TestFinished(referenceID string) ***REMOVED***
+	url := fmt.Sprintf("%s/tests/%s", c.baseURL, referenceID)
+
+	data := struct ***REMOVED***
+		Status int `json:"status"`
+	***REMOVED******REMOVED***
+		1,
+	***REMOVED***
+
+	req, err := c.NewRequest("POST", url, data)
+	if err != nil ***REMOVED***
+		return
+	***REMOVED***
+
+	err = c.Do(req, nil)
+	if err != nil ***REMOVED***
+		return
+	***REMOVED***
+***REMOVED***
+
+type Sample struct ***REMOVED***
+	Type   string     `json:"type"`
+	Metric string     `json:"metric"`
+	Data   SampleData `json:"data"`
+***REMOVED***
+
+type SampleData struct ***REMOVED***
+	Type  stats.MetricType  `json:"type"`
+	Time  time.Time         `json:"time"`
+	Value float64           `json:"value"`
+	Tags  map[string]string `json:"tags,omitempty"`
+***REMOVED***
---
+++ stats/cloud/collector.go
@@ -0,0 +1,90 @@
+package cloud
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/loadimpact/k6/lib"
+	"github.com/loadimpact/k6/stats"
+)
+
+// Collector sends results data to the Load Impact cloud service.
+type Collector struct ***REMOVED***
+	referenceID string
+
+	thresholds map[string][]string
+	client     *Client
+***REMOVED***
+
+func New(fname string, opts lib.Options) (*Collector, error) ***REMOVED***
+	referenceID := os.Getenv("K6CLOUD_REFERENCEID")
+	token := os.Getenv("K6CLOUD_TOKEN")
+
+	thresholds := make(map[string][]string)
+
+	for name, t := range opts.Thresholds ***REMOVED***
+		for _, threshold := range t.Thresholds ***REMOVED***
+			thresholds[name] = append(thresholds[name], threshold.Source)
+		***REMOVED***
+	***REMOVED***
+
+	return &Collector***REMOVED***
+		referenceID: referenceID,
+		thresholds:  thresholds,
+		client:      NewClient(token),
+	***REMOVED***, nil
+***REMOVED***
+
+func (c *Collector) Init() ***REMOVED***
+	name := os.Getenv("K6CLOUD_NAME")
+	if name == "" ***REMOVED***
+		name = "k6 test"
+	***REMOVED***
+
+	// TODO fix this and add proper error handling
+	if c.referenceID == "" ***REMOVED***
+		response := c.client.CreateTestRun(name, c.thresholds)
+		if response != nil ***REMOVED***
+			c.referenceID = response.ReferenceID
+		***REMOVED***
+	***REMOVED***
+***REMOVED***
+
+func (c *Collector) String() string ***REMOVED***
+	return fmt.Sprintf("Load Impact (https://app.staging.loadimpact.com/k6/runs/%s)", c.referenceID)
+***REMOVED***
+
+func (c *Collector) Run(ctx context.Context) ***REMOVED***
+	t := time.Now()
+	<-ctx.Done()
+	s := time.Now()
+
+	c.client.TestFinished(c.referenceID)
+
+	log.Debug(fmt.Sprintf("http://localhost:5000/v1/metrics/%s/%d000/%d000\n", c.referenceID, t.Unix(), s.Unix()))
+***REMOVED***
+
+func (c *Collector) Collect(samples []stats.Sample) ***REMOVED***
+
+	var cloudSamples []*Sample
+	for _, sample := range samples ***REMOVED***
+		sampleJSON := &Sample***REMOVED***
+			Type:   "Point",
+			Metric: sample.Metric.Name,
+			Data: SampleData***REMOVED***
+				Type:  sample.Metric.Type,
+				Time:  sample.Time,
+				Value: sample.Value,
+				Tags:  sample.Tags,
+			***REMOVED***,
+		***REMOVED***
+		cloudSamples = append(cloudSamples, sampleJSON)
+	***REMOVED***
+
+	if len(cloudSamples) > 0 && c.referenceID != "" ***REMOVED***
+		c.client.PushMetric(c.referenceID, cloudSamples)
+	***REMOVED***
+***REMOVED***
