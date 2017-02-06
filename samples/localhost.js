02d693acd530af1a4523aaf1837c5c0c9862e2e5
uppfinnarn <uppfinnarn@gmail.com>
2017-02-06 19:53:50 +0100

    [fix] samples/localhost.js used old GET syntax

--- samples/localhost.js
+++ samples/localhost.js
@@ -11,21 +11,21 @@ export let options = ***REMOVED***
 
 export default function() ***REMOVED***
 	group("front page", function() ***REMOVED***
-		check(http.get("http://localhost:8080/", null, ***REMOVED***
+		check(http.get("http://localhost:8080/", ***REMOVED***
 			tags: ***REMOVED***'kind': 'html' ***REMOVED***,
 		***REMOVED***), ***REMOVED***
 			"status is 200": (res) => res.status === 200,
 		***REMOVED***);
 	***REMOVED***);
 	group("stylesheet", function() ***REMOVED***
-		check(http.get("http://localhost:8080/style.css", null, ***REMOVED***
+		check(http.get("http://localhost:8080/style.css", ***REMOVED***
 			tags: ***REMOVED***'kind': 'css' ***REMOVED***,
 		***REMOVED***), ***REMOVED***
 			"status is 200": (res) => res.status === 200,
 		***REMOVED***);
 	***REMOVED***);
 	group("image", function() ***REMOVED***
-		check(http.get("http://localhost:8080/teddy.jpg", null, ***REMOVED***
+		check(http.get("http://localhost:8080/teddy.jpg", ***REMOVED***
 			tags: ***REMOVED***'kind': 'img' ***REMOVED***,
 		***REMOVED***), ***REMOVED***
 			"status is 200": (res) => res.status === 200,
