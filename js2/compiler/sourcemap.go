1ed951eb7380f590f9c767a4ab79f5d9ce0cf203
uppfinnarn <uppfinnarn@gmail.com>
2017-02-13 22:14:09 +0100

    Babel-powered, in-process ES6 compiler w/ maps
    Related: #43

---
+++ js2/compiler/sourcemap.go
@@ -0,0 +1,10 @@
+package compiler
+
+type SourceMap struct ***REMOVED***
+	Version    int
+	File       string
+	SourceRoot string
+	Sources    []string
+	Names      []string
+	Mappings   string
+***REMOVED***
