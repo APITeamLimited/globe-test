ef80402f5ab17bc4b561dff791394802a529c45c
penny <penny@penny.lgbt>
2017-02-19 17:48:43 +0100

    Extend sample with object batch argument

--- samples/http_batch.js
+++ samples/http_batch.js
@@ -5,6 +5,10 @@ export default function() ***REMOVED***
   const responses = http.batch([
     "http://test.loadimpact.com",
     "http://test.loadimpact.com/pi.php",
+    ***REMOVED***
+      "method": "GET",
+      "url": "http://test.loadimpact.com/pi.php?decimals=50"
+    ***REMOVED***
   ]);
 
   check(responses[0], ***REMOVED***
@@ -13,5 +17,11 @@ export default function() ***REMOVED***
 
   check(responses[1], ***REMOVED***
     "pi page 200": res => res.status === 200,
+  ***REMOVED***)
+
+  check(responses[2], ***REMOVED***
+    "pi page has 50 digits": res => ***REMOVED***
+      return res.body === "3.14159265358979323846264338327950288"
+    ***REMOVED***
   ***REMOVED***)
 ***REMOVED***;
