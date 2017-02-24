9dcfd2de962aeedfa683d9efb292924fef627912
Ragnar Lonn <ragnar@loadimpact.com>
2017-02-24 15:15:28 +0100

    Expose effective URL in HTTP response
    Closes #133

--- js/api_http.go
+++ js/api_http.go
@@ -96,9 +96,10 @@ func (a JSAPI) HTTPRequest(method, url, body string, paramData string) map[strin
 		headers[k] = strings.Join(v, ", ")
 	***REMOVED***
 	return map[string]interface***REMOVED******REMOVED******REMOVED***
-		"status":  res.StatusCode,
-		"body":    string(resBody),
-		"headers": headers,
+		"effective_url": res.Request.URL.String(),
+		"status":        res.StatusCode,
+		"body":          string(resBody),
+		"headers":       headers,
 		"timings": map[string]float64***REMOVED***
 			"duration":   stats.D(trail.Duration),
 			"blocked":    stats.D(trail.Blocked),
