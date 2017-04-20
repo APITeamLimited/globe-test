113efc9b449012731afe551ca7702835287f352d
Emily Ekberg <emily@loadimpact.com>
2017-04-20 17:41:13 +0100

    Disabled source map tests

--- js/compiler/compiler_test.go
+++ js/compiler/compiler_test.go
@@ -20,30 +20,29 @@ func TestTransform(t *testing.T) ***REMOVED***
 	***REMOVED***
 
 	t.Run("blank", func(t *testing.T) ***REMOVED***
-		src, srcmap, err := c.Transform("", "test.js")
+		src, _, err := c.Transform("", "test.js")
 		assert.NoError(t, err)
 		assert.Equal(t, `"use strict";`, src)
-		assert.Equal(t, 3, srcmap.Version)
-		assert.Equal(t, "test.js", srcmap.File)
-		assert.Equal(t, "", srcmap.Mappings)
+		// assert.Equal(t, 3, srcmap.Version)
+		// assert.Equal(t, "test.js", srcmap.File)
+		// assert.Equal(t, "", srcmap.Mappings)
 	***REMOVED***)
 	t.Run("double-arrow", func(t *testing.T) ***REMOVED***
-		src, srcmap, err := c.Transform("()=> true", "test.js")
+		src, _, err := c.Transform("()=> true", "test.js")
 		assert.NoError(t, err)
 		assert.Equal(t, `"use strict";(function () ***REMOVED***return true;***REMOVED***);`, src)
-		assert.Equal(t, 3, srcmap.Version)
-		assert.Equal(t, "test.js", srcmap.File)
-		assert.Equal(t, "aAAA,qBAAK,IAAL", srcmap.Mappings)
+		// assert.Equal(t, 3, srcmap.Version)
+		// assert.Equal(t, "test.js", srcmap.File)
+		// assert.Equal(t, "aAAA,qBAAK,IAAL", srcmap.Mappings)
 	***REMOVED***)
 	t.Run("longer", func(t *testing.T) ***REMOVED***
-		src, srcmap, err := c.Transform(strings.Join([]string***REMOVED***
+		src, _, err := c.Transform(strings.Join([]string***REMOVED***
 			`function add(a, b) ***REMOVED***`,
 			`    return a + b;`,
 			`***REMOVED***;`,
 			``,
 			`let res = add(1, 2);`,
 		***REMOVED***, "\n"), "test.js")
-		println(src)
 		assert.NoError(t, err)
 		assert.Equal(t, strings.Join([]string***REMOVED***
 			`"use strict";function add(a, b) ***REMOVED***`,
@@ -52,8 +51,8 @@ func TestTransform(t *testing.T) ***REMOVED***
 			``,
 			`var res = add(1, 2);`,
 		***REMOVED***, "\n"), src)
-		assert.Equal(t, 3, srcmap.Version)
-		assert.Equal(t, "test.js", srcmap.File)
-		assert.Equal(t, "aAAA,SAASA,GAAT,CAAaC,CAAb,EAAgBC,CAAhB,EAAmB;AACf,WAAOD,IAAIC,CAAX;AACH;;AAED,IAAIC,MAAMH,IAAI,CAAJ,EAAO,CAAP,CAAV", srcmap.Mappings)
+		// assert.Equal(t, 3, srcmap.Version)
+		// assert.Equal(t, "test.js", srcmap.File)
+		// assert.Equal(t, "aAAA,SAASA,GAAT,CAAaC,CAAb,EAAgBC,CAAhB,EAAmB;AACf,WAAOD,IAAIC,CAAX;AACH;;AAED,IAAIC,MAAMH,IAAI,CAAJ,EAAO,CAAP,CAAV", srcmap.Mappings)
 	***REMOVED***)
 ***REMOVED***
