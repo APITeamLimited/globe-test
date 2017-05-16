a0acc7fbea6a5a9d1d0393165b967cf0416ffd11
Emily Ekberg <emily@loadimpact.com>
2017-05-16 18:09:08 +0100

    You can now 'k6 run' an archive

--- run.go
+++ run.go
@@ -21,8 +21,8 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -38,6 +38,8 @@ import (
 	"syscall"
 	"time"
 
+	"archive/tar"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/fatih/color"
 	"github.com/ghodss/yaml"
@@ -51,15 +53,17 @@ import (
 	"github.com/loadimpact/k6/stats/influxdb"
 	"github.com/loadimpact/k6/stats/json"
 	"github.com/loadimpact/k6/ui"
+	"github.com/pkg/errors"
 	"github.com/spf13/afero"
 	"gopkg.in/guregu/null.v3"
 	"gopkg.in/urfave/cli.v1"
 )
 
 const (
-	TypeAuto = "auto"
-	TypeURL  = "url"
-	TypeJS   = "js"
+	TypeAuto    = "auto"
+	TypeURL     = "url"
+	TypeJS      = "js"
+	TypeArchive = "archive"
 )
 
 var urlRegex = regexp.MustCompile(`(?i)^https?://`)
@@ -92,7 +96,7 @@ var optionFlags = []cli.Flag***REMOVED***
 	***REMOVED***,
 	cli.StringFlag***REMOVED***
 		Name:  "type, t",
-		Usage: "input type, one of: auto, url, js",
+		Usage: "input type, one of: auto, url, js, archive",
 		Value: "auto",
 	***REMOVED***,
 	cli.BoolFlag***REMOVED***
@@ -141,10 +145,6 @@ var commandRun = cli.Command***REMOVED***
 			Usage:  "output metrics to an external data store (format: type=uri)",
 			EnvVar: "K6_OUT",
 		***REMOVED***,
-		cli.StringFlag***REMOVED***
-			Name:  "archive, a",
-			Usage: "Run an archive",
-		***REMOVED***,
 	),
 	Action: actionRun,
 	Description: `Run starts a load test.
@@ -186,7 +186,7 @@ var commandInspect = cli.Command***REMOVED***
 	Flags: []cli.Flag***REMOVED***
 		cli.StringFlag***REMOVED***
 			Name:  "type, t",
-			Usage: "input type, one of: auto, url, js",
+			Usage: "input type, one of: auto, url, js, archive",
 			Value: "auto",
 		***REMOVED***,
 	***REMOVED***,
@@ -194,8 +194,13 @@ var commandInspect = cli.Command***REMOVED***
 ***REMOVED***
 
 func guessType(data []byte) string ***REMOVED***
+	// See if it looks like a URL.
 	if urlRegex.Match(data) ***REMOVED***
 		return TypeURL
+	***REMOVED***
+	// See if it has a valid tar header.
+	if _, err := tar.NewReader(bytes.NewReader(data)).Next(); err == nil ***REMOVED***
+		return TypeArchive
 	***REMOVED***
 	return TypeJS
 ***REMOVED***
@@ -240,6 +245,17 @@ func makeRunner(runnerType string, src *lib.SourceData, fs afero.Fs) (lib.Runner
 		return r, err
 	case TypeJS:
 		return js.New(src, fs)
+	case TypeArchive:
+		arc, err := lib.ReadArchive(bytes.NewReader(src.Data))
+		if err != nil ***REMOVED***
+			return nil, err
+		***REMOVED***
+		switch arc.Type ***REMOVED***
+		case TypeJS:
+			return js.NewFromArchive(arc)
+		default:
+			return nil, errors.Errorf("Invalid archive - unrecognized type: '%s'", arc.Type)
+		***REMOVED***
 	default:
 		return nil, errors.New("Invalid type specified, see --help")
 	***REMOVED***
