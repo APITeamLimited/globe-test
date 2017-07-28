4f3f6f0f1987f898c1e9aebe6d08087e690d7358
Tim Hunt <tnhunt@gmail.com>
2017-07-28 18:57:24 +0100

    Use sel.URL in res.Html()

--- js/modules/k6/http/http.go
+++ js/modules/k6/http/http.go
@@ -140,7 +140,7 @@ func (res *HTTPResponse) Html(selector ...string) html.Selection ***REMOVED***
 	if err != nil ***REMOVED***
 		common.Throw(common.GetRuntime(res.ctx), err)
 	***REMOVED***
-	sel.Url = res.URL
+	sel.URL = res.URL
 	if len(selector) > 0 ***REMOVED***
 		sel = sel.Find(selector[0])
 >>>>>>> Make accessors for attributes which contain urls behave similarly to DOM API.
