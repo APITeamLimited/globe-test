97a1da7a6811185d52c5b12c74998d3854c5b865
aidylewis <adrian_lewis@hushmail.com>
2017-09-18 10:58:20 +0100

    HTTP/2 check on BBC Homepage

---
+++ samples/http_2.js
@@ -0,0 +1,9 @@
+import http from "k6/http";
+import ***REMOVED*** check ***REMOVED*** from "k6";
+
+export default function() ***REMOVED***
+  check(http.get("https://www.bbc.co.uk/"), ***REMOVED***
+    "status is 200": (r) => r.status == 200,
+    "protocol is HTTP/2": (r) => r.proto == "HTTP/2.0",
+  ***REMOVED***);
+***REMOVED***
