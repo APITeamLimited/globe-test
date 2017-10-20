b5a7f971be99143bd2ca9eff6369124a45305629
Robin Gustafsson <robin@loadimpact.com>
2017-10-20 15:11:49 +0200

    Fix examples

--- samples/ocsp.js
+++ samples/ocsp.js
@@ -4,6 +4,6 @@ import ***REMOVED*** check ***REMOVED*** from "k6";
 export default function() ***REMOVED***
     let res = http.get("https://stackoverflow.com");
     check(res, ***REMOVED***
-        "is OCSP response good": (r) => r.ocsp.stapled_response.status === "good"
+        "is OCSP response good": (r) => r.ocsp.status === http.OCSP_STATUS_GOOD
     ***REMOVED***);
 ***REMOVED***;
