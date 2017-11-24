cea3ef785fce006334dfc540393924c96bc46bd0
Borja Campina <borjacampina@gmail.com>
2017-11-24 11:22:04 +0100

    Multistage k6 Dockerfile, add code coverage

---
+++ vendor/github.com/stretchr/testify/assert/http_assertions.go
@@ -0,0 +1,127 @@
+package assert
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+)
+
+// httpCode is a helper that returns HTTP code of the response. It returns -1 and
+// an error if building a new request fails.
+func httpCode(handler http.HandlerFunc, method, url string, values url.Values) (int, error) ***REMOVED***
+	w := httptest.NewRecorder()
+	req, err := http.NewRequest(method, url+"?"+values.Encode(), nil)
+	if err != nil ***REMOVED***
+		return -1, err
+	***REMOVED***
+	handler(w, req)
+	return w.Code, nil
+***REMOVED***
+
+// HTTPSuccess asserts that a specified handler returns a success status code.
+//
+//  assert.HTTPSuccess(t, myHandler, "POST", "http://www.google.com", nil)
+//
+// Returns whether the assertion was successful (true) or not (false).
+func HTTPSuccess(t TestingT, handler http.HandlerFunc, method, url string, values url.Values) bool ***REMOVED***
+	code, err := httpCode(handler, method, url, values)
+	if err != nil ***REMOVED***
+		Fail(t, fmt.Sprintf("Failed to build test request, got error: %s", err))
+		return false
+	***REMOVED***
+
+	isSuccessCode := code >= http.StatusOK && code <= http.StatusPartialContent
+	if !isSuccessCode ***REMOVED***
+		Fail(t, fmt.Sprintf("Expected HTTP success status code for %q but received %d", url+"?"+values.Encode(), code))
+	***REMOVED***
+
+	return isSuccessCode
+***REMOVED***
+
+// HTTPRedirect asserts that a specified handler returns a redirect status code.
+//
+//  assert.HTTPRedirect(t, myHandler, "GET", "/a/b/c", url.Values***REMOVED***"a": []string***REMOVED***"b", "c"***REMOVED******REMOVED***
+//
+// Returns whether the assertion was successful (true) or not (false).
+func HTTPRedirect(t TestingT, handler http.HandlerFunc, method, url string, values url.Values) bool ***REMOVED***
+	code, err := httpCode(handler, method, url, values)
+	if err != nil ***REMOVED***
+		Fail(t, fmt.Sprintf("Failed to build test request, got error: %s", err))
+		return false
+	***REMOVED***
+
+	isRedirectCode := code >= http.StatusMultipleChoices && code <= http.StatusTemporaryRedirect
+	if !isRedirectCode ***REMOVED***
+		Fail(t, fmt.Sprintf("Expected HTTP redirect status code for %q but received %d", url+"?"+values.Encode(), code))
+	***REMOVED***
+
+	return isRedirectCode
+***REMOVED***
+
+// HTTPError asserts that a specified handler returns an error status code.
+//
+//  assert.HTTPError(t, myHandler, "POST", "/a/b/c", url.Values***REMOVED***"a": []string***REMOVED***"b", "c"***REMOVED******REMOVED***
+//
+// Returns whether the assertion was successful (true) or not (false).
+func HTTPError(t TestingT, handler http.HandlerFunc, method, url string, values url.Values) bool ***REMOVED***
+	code, err := httpCode(handler, method, url, values)
+	if err != nil ***REMOVED***
+		Fail(t, fmt.Sprintf("Failed to build test request, got error: %s", err))
+		return false
+	***REMOVED***
+
+	isErrorCode := code >= http.StatusBadRequest
+	if !isErrorCode ***REMOVED***
+		Fail(t, fmt.Sprintf("Expected HTTP error status code for %q but received %d", url+"?"+values.Encode(), code))
+	***REMOVED***
+
+	return isErrorCode
+***REMOVED***
+
+// HTTPBody is a helper that returns HTTP body of the response. It returns
+// empty string if building a new request fails.
+func HTTPBody(handler http.HandlerFunc, method, url string, values url.Values) string ***REMOVED***
+	w := httptest.NewRecorder()
+	req, err := http.NewRequest(method, url+"?"+values.Encode(), nil)
+	if err != nil ***REMOVED***
+		return ""
+	***REMOVED***
+	handler(w, req)
+	return w.Body.String()
+***REMOVED***
+
+// HTTPBodyContains asserts that a specified handler returns a
+// body that contains a string.
+//
+//  assert.HTTPBodyContains(t, myHandler, "www.google.com", nil, "I'm Feeling Lucky")
+//
+// Returns whether the assertion was successful (true) or not (false).
+func HTTPBodyContains(t TestingT, handler http.HandlerFunc, method, url string, values url.Values, str interface***REMOVED******REMOVED***) bool ***REMOVED***
+	body := HTTPBody(handler, method, url, values)
+
+	contains := strings.Contains(body, fmt.Sprint(str))
+	if !contains ***REMOVED***
+		Fail(t, fmt.Sprintf("Expected response body for \"%s\" to contain \"%s\" but found \"%s\"", url+"?"+values.Encode(), str, body))
+	***REMOVED***
+
+	return contains
+***REMOVED***
+
+// HTTPBodyNotContains asserts that a specified handler returns a
+// body that does not contain a string.
+//
+//  assert.HTTPBodyNotContains(t, myHandler, "www.google.com", nil, "I'm Feeling Lucky")
+//
+// Returns whether the assertion was successful (true) or not (false).
+func HTTPBodyNotContains(t TestingT, handler http.HandlerFunc, method, url string, values url.Values, str interface***REMOVED******REMOVED***) bool ***REMOVED***
+	body := HTTPBody(handler, method, url, values)
+
+	contains := strings.Contains(body, fmt.Sprint(str))
+	if contains ***REMOVED***
+		Fail(t, fmt.Sprintf("Expected response body for \"%s\" to NOT contain \"%s\" but found \"%s\"", url+"?"+values.Encode(), str, body))
+	***REMOVED***
+
+	return !contains
+***REMOVED***
