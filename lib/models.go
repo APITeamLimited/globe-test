ce8e388fad08267fd601e1d40872f4941b2d0b17
Emily Ekberg <emily@loadimpact.com>
2017-11-30 18:42:43 +0000

    Why did these have JSON tags...

--- lib/models.go
+++ lib/models.go
@@ -92,8 +92,8 @@ type Group struct ***REMOVED***
 	Groups map[string]*Group `json:"groups"`
 	Checks map[string]*Check `json:"checks"`
 
-	groupMutex sync.Mutex `json:"-"`
-	checkMutex sync.Mutex `json:"-"`
+	groupMutex sync.Mutex
+	checkMutex sync.Mutex
 ***REMOVED***
 
 func NewGroup(name string, parent *Group) (*Group, error) ***REMOVED***
