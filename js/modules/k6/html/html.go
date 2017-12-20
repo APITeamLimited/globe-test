64bb42fe1ea3deac21ae44f74c5d679b5902e630
Mark Lagendijk <mark@lagendijk.nl>
2017-12-20 14:06:49 +0100

    Implemented most of #424: - SerializeArray - SerializeObject - Serialize
    ToDo
    - Find out how to get the value of the form elements. Preferably by using an existing function (E.g. element.Val())
    - Find out how to handle arrays of values (<select multiple>. This would make 'value' either an string or an array of key/value.

--- js/modules/k6/html/html.go
+++ js/modules/k6/html/html.go
@@ -23,6 +23,8 @@ package html
 import (
 	"context"
 	"fmt"
+	neturl "net/url"
+	"regexp"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -50,6 +52,11 @@ type Selection struct ***REMOVED***
 	rt  *goja.Runtime
 	sel *goquery.Selection
 	URL string
+***REMOVED***
+
+type FormValue struct ***REMOVED***
+	name  string
+	value string
 ***REMOVED***
 
 func (s Selection) emptySelection() Selection ***REMOVED***
@@ -451,4 +458,63 @@ func (s Selection) Data(def ...string) goja.Value ***REMOVED***
 		***REMOVED***
 		return s.rt.ToValue(data)
 	***REMOVED***
+***REMOVED***
+
+func (s Selection) SerializeArray() []FormValue ***REMOVED***
+	submittableSelector := "input,select,textarea,keygen"
+	var formElements *goquery.Selection
+	if s.sel.Is("form") ***REMOVED***
+		formElements = s.sel.Find(submittableSelector)
+	***REMOVED*** else ***REMOVED***
+		formElements = s.sel.Filter(submittableSelector)
+	***REMOVED***
+
+	formElements = formElements.FilterFunction(func(i int, sel *goquery.Selection) bool ***REMOVED***
+		name := sel.AttrOr("name", "")
+		inputType := sel.AttrOr("type", "")
+		disabled := sel.AttrOr("disabled", "")
+		checked := sel.AttrOr("checked", "")
+
+		return name != "" && // Must have a non-empty name
+			disabled != "disabled" && // Must not be disabled
+			inputType != "submit" && // Must not be a button
+			inputType != "button" &&
+			inputType != "reset" &&
+			inputType != "image" && // Must not be an image or file
+			inputType != "file" &&
+			(checked == "checked" || (inputType != "checkbox" && inputType != "radio" )) // Must be checked if it is an checkbox or radio
+	***REMOVED***)
+
+	reCRLF := regexp.MustCompile("\r?\n")
+	result := make([]FormValue, len(formElements.Nodes))
+	formElements.Each(func(i int, sel *goquery.Selection) ***REMOVED***
+		name, _ := sel.Attr("name")
+		// ToDo: find a way to use something like 'sel.Val()' or implement it here
+		value := reCRLF.ReplaceAllString(sel.AttrOr("value", ""), "\r\n")
+		result[i] = FormValue***REMOVED***name: name, value: value***REMOVED***
+
+		// ToDo: If we have an array of values (e.g. `<select multiple>`), return an array of key/value pairs
+		// This would make FormValue.value either an string or an array of ***REMOVED*** name, value ***REMOVED***. 
+	***REMOVED***)
+	return result
+***REMOVED***
+
+func (s Selection) SerializeObject() map[string]string ***REMOVED***
+	formValues := s.SerializeArray()
+	result := make(map[string]string)
+	for i := range formValues ***REMOVED***
+		formValue := formValues[i]
+		result[formValue.name] = formValue.value
+	***REMOVED***
+	return result
+***REMOVED***
+
+func (s Selection) Serialize() string ***REMOVED***
+	formValues := s.SerializeArray()
+	urlValues := make(neturl.Values, len(formValues))
+	for i := range formValues ***REMOVED***
+		formValue := formValues[i]
+		urlValues.Set(formValue.name, formValue.value)
+	***REMOVED***
+	return urlValues.Encode()
 ***REMOVED***
