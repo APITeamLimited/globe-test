1a3769e4f671a6bb802ec322e524f8863f0770ec
Mark Lagendijk <mark.lagendijk@connectis.nl>
2017-12-21 11:08:33 +0100

    Moved serialization methods to own serialize.go.

--- js/modules/k6/html/html.go
+++ js/modules/k6/html/html.go
@@ -23,7 +23,6 @@ package html
 import (
 	"context"
 	"fmt"
-	neturl "net/url"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -51,11 +50,6 @@ type Selection struct ***REMOVED***
 	rt  *goja.Runtime
 	sel *goquery.Selection
 	URL string
-***REMOVED***
-
-type FormValue struct ***REMOVED***
-	Name  string
-	Value goja.Value
 ***REMOVED***
 
 func (s Selection) emptySelection() Selection ***REMOVED***
@@ -457,67 +451,4 @@ func (s Selection) Data(def ...string) goja.Value ***REMOVED***
 		***REMOVED***
 		return s.rt.ToValue(data)
 	***REMOVED***
-***REMOVED***
-
-// nolint: goconst
-func (s Selection) SerializeArray() []FormValue ***REMOVED***
-	submittableSelector := "input,select,textarea,keygen"
-	var formElements *goquery.Selection
-	if s.sel.Is("form") ***REMOVED***
-		formElements = s.sel.Find(submittableSelector)
-	***REMOVED*** else ***REMOVED***
-		formElements = s.sel.Filter(submittableSelector)
-	***REMOVED***
-
-	formElements = formElements.FilterFunction(func(i int, sel *goquery.Selection) bool ***REMOVED***
-		name := sel.AttrOr("name", "")
-		inputType := sel.AttrOr("type", "")
-		disabled := sel.AttrOr("disabled", "")
-		checked := sel.AttrOr("checked", "")
-
-		return name != "" && // Must have a non-empty name
-			disabled != "disabled" && // Must not be disabled
-			inputType != "submit" && // Must not be a button
-			inputType != "button" &&
-			inputType != "reset" &&
-			inputType != "image" && // Must not be an image or file
-			inputType != "file" &&
-			(checked == "checked" || (inputType != "checkbox" && inputType != "radio")) // Must be checked if it is an checkbox or radio
-	***REMOVED***)
-
-	result := make([]FormValue, len(formElements.Nodes))
-	formElements.Each(func(i int, sel *goquery.Selection) ***REMOVED***
-		element := Selection***REMOVED***s.rt, sel, s.URL***REMOVED***
-		name, _ := sel.Attr("name")
-		result[i] = FormValue***REMOVED***Name: name, Value: element.Val()***REMOVED***
-	***REMOVED***)
-	return result
-***REMOVED***
-
-func (s Selection) SerializeObject() map[string]goja.Value ***REMOVED***
-	formValues := s.SerializeArray()
-	result := make(map[string]goja.Value)
-	for i := range formValues ***REMOVED***
-		formValue := formValues[i]
-		result[formValue.Name] = formValue.Value
-	***REMOVED***
-
-	return result
-***REMOVED***
-
-func (s Selection) Serialize() string ***REMOVED***
-	formValues := s.SerializeArray()
-	urlValues := make(neturl.Values, len(formValues))
-	for i := range formValues ***REMOVED***
-		formValue := formValues[i]
-		value := formValue.Value.Export()
-		switch value.(type) ***REMOVED***
-		case string:
-			urlValues.Set(formValue.Name, value.(string))
-		case []string:
-			urlValues[formValue.Name] = value.([]string)
-		***REMOVED***
-
-	***REMOVED***
-	return urlValues.Encode()
 ***REMOVED***
