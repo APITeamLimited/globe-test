c1b41b68b000f0bd7cd3451456e54f6c5c528a50
Callum Styan <callumstyan@gmail.com>
2018-01-18 22:46:34 -0800

    refactor response structs handling of the requests body, add test

--- js/modules/k6/http/http_request_test.go
+++ js/modules/k6/http/http_request_test.go
@@ -864,25 +864,35 @@ func TestRequestAndBatch(t *testing.T) ***REMOVED***
 	***REMOVED***)
 
 	t.Run("HTTPRequest", func(t *testing.T) ***REMOVED***
-		_, err := common.RunString(rt, `
-			let reqUrl = "https://httpbin.org/cookies"
-			let res = http.get(reqUrl);
-			let jar = new http.CookieJar();
+		t.Run("EmptyBody", func(t *testing.T) ***REMOVED***
+			_, err := common.RunString(rt, `
+				let reqUrl = "https://httpbin.org/cookies"
+				let res = http.get(reqUrl);
+				let jar = new http.CookieJar();
 
-			jar.set("https://httpbin.org/cookies", "key", "value");
-			res = http.request("GET", "https://httpbin.org/cookies", null, ***REMOVED*** cookies: ***REMOVED*** key2: "value2" ***REMOVED***, jar: jar ***REMOVED***);
+				jar.set("https://httpbin.org/cookies", "key", "value");
+				res = http.request("GET", "https://httpbin.org/cookies", null, ***REMOVED*** cookies: ***REMOVED*** key2: "value2" ***REMOVED***, jar: jar ***REMOVED***);
 
-			if (res.json().cookies.key != "value") ***REMOVED*** throw new Error("wrong cookie value: " + res.json().cookies.key); ***REMOVED***
+				if (res.json().cookies.key != "value") ***REMOVED*** throw new Error("wrong cookie value: " + res.json().cookies.key); ***REMOVED***
 
-			if (res.status != 200) ***REMOVED*** throw new Error("wrong status: " + res.status); ***REMOVED***
-			if (res.request["method"] !== "GET") ***REMOVED*** throw new Error("http request method was not \"GET\": " + JSON.stringify(res.request)) ***REMOVED***
-			if (res.request["body"] != null) ***REMOVED*** throw new Error("http request body was not null: " + JSON.stringify(res.request)) ***REMOVED***
-			if (res.request["url"] != reqUrl) ***REMOVED***
-				throw new Error("wront http request url: " + JSON.stringify(res.request))
-			***REMOVED***
-			if (res.request["cookies"]["key2"][0].name != "key2") ***REMOVED*** throw new Error("wrong http request cookies: " + JSON.stringify(JSON.stringify(res.request["cookies"]["key2"]))) ***REMOVED***
-			if (res.request["headers"]["User-Agent"][0] != "TestUserAgent") ***REMOVED*** throw new Error("wrong http request headers: " + JSON.stringify(res.request)) ***REMOVED***
-			`)
-		assert.NoError(t, err)
+				if (res.status != 200) ***REMOVED*** throw new Error("wrong status: " + res.status); ***REMOVED***
+				if (res.request["method"] !== "GET") ***REMOVED*** throw new Error("http request method was not \"GET\": " + JSON.stringify(res.request)) ***REMOVED***
+				if (res.request["body"].length != 0) ***REMOVED*** throw new Error("http request body was not null: " + JSON.stringify(res.request["body"])) ***REMOVED***
+				if (res.request["url"] != reqUrl) ***REMOVED***
+					throw new Error("wrong http request url: " + JSON.stringify(res.request))
+				***REMOVED***
+				if (res.request["cookies"]["key2"][0].name != "key2") ***REMOVED*** throw new Error("wrong http request cookies: " + JSON.stringify(JSON.stringify(res.request["cookies"]["key2"]))) ***REMOVED***
+				if (res.request["headers"]["User-Agent"][0] != "TestUserAgent") ***REMOVED*** throw new Error("wrong http request headers: " + JSON.stringify(res.request)) ***REMOVED***
+				`)
+			assert.NoError(t, err)
+		***REMOVED***)
+		t.Run("NonEmptyBody", func(t *testing.T) ***REMOVED***
+			_, err := common.RunString(rt, `
+				let res = http.post("https://httpbin.org/post", ***REMOVED***a: "a", b: 2***REMOVED***, ***REMOVED***headers: ***REMOVED***"Content-Type": "application/x-www-form-urlencoded; charset=utf-8"***REMOVED******REMOVED***);
+				if (res.status != 200) ***REMOVED*** throw new Error("wrong status: " + res.status); ***REMOVED***
+				if (res.request["body"] != "a=a&b=2") ***REMOVED*** throw new Error("http request body was not set properly: " + JSON.stringify(res.request))***REMOVED***
+				`)
+			assert.NoError(t, err)
+		***REMOVED***)
 	***REMOVED***)
 ***REMOVED***
