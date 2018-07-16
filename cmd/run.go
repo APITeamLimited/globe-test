d10120608bbab29caf0f5b33751879c55412148c
Nedyalko Andreev <n@andreev.sh>
2018-07-16 14:10:48 +0300

    Tie lib.Options.VUsMax's "validity" to that of VUs

--- cmd/run.go
+++ cmd/run.go
@@ -141,7 +141,7 @@ a commandline interface for interacting with it.`,
 
 		// If -m/--max isn't specified, figure out the max that should be needed.
 		if !conf.VUsMax.Valid ***REMOVED***
-			conf.VUsMax = null.IntFrom(conf.VUs.Int64)
+			conf.VUsMax = null.NewInt(conf.VUs.Int64, conf.VUs.Valid)
 			for _, stage := range conf.Stages ***REMOVED***
 				if stage.Target.Valid && stage.Target.Int64 > conf.VUsMax.Int64 ***REMOVED***
 					conf.VUsMax = stage.Target
