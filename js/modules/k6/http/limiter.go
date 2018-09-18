d86937fd88d2df9f82c054e371e32f98beeab0b0
na-- <na--@users.noreply.github.com>
2018-09-18 16:26:32 +0300

    Fix, refactor, document and test the slot limiters (#771)
    Fix, refactor, document and test the slot limiters and update the release notes
    
    This fixes https://github.com/loadimpact/k6/issues/770

--- js/modules/k6/http/limiter.go
+++ js/modules/k6/http/limiter.go
@@ -20,51 +20,69 @@
 
 package http
 
-type SlotLimiter struct ***REMOVED***
-	ch chan struct***REMOVED******REMOVED***
-***REMOVED***
+import (
+	"sync"
+)
 
+// SlotLimiter can restrict the concurrent execution of tasks to the given `slots` limit
+type SlotLimiter chan struct***REMOVED******REMOVED***
+
+// NewSlotLimiter initializes and returns a new SlotLimiter with the given slot count
 func NewSlotLimiter(slots int) SlotLimiter ***REMOVED***
 	if slots <= 0 ***REMOVED***
-		return SlotLimiter***REMOVED***nil***REMOVED***
+		return nil
 	***REMOVED***
 
 	ch := make(chan struct***REMOVED******REMOVED***, slots)
 	for i := 0; i < slots; i++ ***REMOVED***
 		ch <- struct***REMOVED******REMOVED******REMOVED******REMOVED***
 	***REMOVED***
-	return SlotLimiter***REMOVED***ch***REMOVED***
+	return ch
 ***REMOVED***
 
-func (l *SlotLimiter) Begin() ***REMOVED***
-	if l.ch != nil ***REMOVED***
-		<-l.ch
+// Begin uses up a slot to denote the start of a task exeuction. It's a noop if the number
+// of slots is 0, and if no slots are available, it blocks and waits.
+func (sl SlotLimiter) Begin() ***REMOVED***
+	if sl != nil ***REMOVED***
+		<-sl
 	***REMOVED***
 ***REMOVED***
 
-func (l *SlotLimiter) End() ***REMOVED***
-	if l.ch != nil ***REMOVED***
-		l.ch <- struct***REMOVED******REMOVED******REMOVED******REMOVED***
+// End restores a slot and should be called at the end of a taks execution, preferably
+// from a defer statement right after Begin()
+func (sl SlotLimiter) End() ***REMOVED***
+	if sl != nil ***REMOVED***
+		sl <- struct***REMOVED******REMOVED******REMOVED******REMOVED***
 	***REMOVED***
 ***REMOVED***
 
+// MultiSlotLimiter can restrict the concurrent execution of different groups of tasks
+// to the given `slots` limit. Each group is represented with a string ID.
 type MultiSlotLimiter struct ***REMOVED***
-	m     map[string]*SlotLimiter
+	m     map[string]SlotLimiter
 	slots int
+	mutex sync.Mutex
 ***REMOVED***
 
-func NewMultiSlotLimiter(slots int) MultiSlotLimiter ***REMOVED***
-	return MultiSlotLimiter***REMOVED***make(map[string]*SlotLimiter), slots***REMOVED***
+// NewMultiSlotLimiter initializes and returns a new MultiSlotLimiter with the given slot count
+func NewMultiSlotLimiter(slots int) *MultiSlotLimiter ***REMOVED***
+	return &MultiSlotLimiter***REMOVED***make(map[string]SlotLimiter), slots, sync.Mutex***REMOVED******REMOVED******REMOVED***
 ***REMOVED***
 
-func (l *MultiSlotLimiter) Slot(s string) *SlotLimiter ***REMOVED***
+// Slot is used to retrieve the corresponding slot to the given string ID. If no slot with that ID exists,
+// it creates it and saves it for future use. It is safe to call this method concurrently.
+func (l *MultiSlotLimiter) Slot(s string) SlotLimiter ***REMOVED***
 	if l.slots == 0 ***REMOVED***
 		return nil
 	***REMOVED***
+
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
 	ll, ok := l.m[s]
 	if !ok ***REMOVED***
 		tmp := NewSlotLimiter(l.slots)
-		ll = &tmp
+		ll = tmp
 		l.m[s] = ll
 	***REMOVED***
 	return ll
--- js/modules/k6/http/limiter_test.go
+++ js/modules/k6/http/limiter_test.go
@@ -20,12 +20,16 @@
 package http
 
 import (
+	"fmt"
+	"sync"
+	"sync/atomic"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSlotLimiter(t *testing.T) ***REMOVED***
+func TestSlotLimiterSingleSlot(t *testing.T) ***REMOVED***
+	t.Parallel()
 	l := NewSlotLimiter(1)
 	l.Begin()
 	done := false
@@ -38,7 +42,67 @@ func TestSlotLimiter(t *testing.T) ***REMOVED***
 	l.End()
 ***REMOVED***
 
+func TestSlotLimiterUnlimited(t *testing.T) ***REMOVED***
+	t.Parallel()
+	l := NewSlotLimiter(0)
+	l.Begin()
+	l.Begin()
+	l.Begin()
+***REMOVED***
+func TestSlotLimiters(t *testing.T) ***REMOVED***
+	t.Parallel()
+	testCases := []struct***REMOVED*** limit, launches, expMid int ***REMOVED******REMOVED***
+		***REMOVED***0, 0, 0***REMOVED***,
+		***REMOVED***0, 1, 1***REMOVED***,
+		***REMOVED***0, 5, 5***REMOVED***,
+		***REMOVED***1, 5, 1***REMOVED***,
+		***REMOVED***2, 5, 2***REMOVED***,
+		***REMOVED***5, 6, 5***REMOVED***,
+		***REMOVED***6, 5, 5***REMOVED***,
+		***REMOVED***10, 7, 7***REMOVED***,
+	***REMOVED***
+
+	for _, tc := range testCases ***REMOVED***
+		tc := tc
+		t.Run(fmt.Sprintf("limit=%d,launches=%d", tc.limit, tc.launches), func(t *testing.T) ***REMOVED***
+			t.Parallel()
+			l := NewSlotLimiter(tc.limit)
+			wg := sync.WaitGroup***REMOVED******REMOVED***
+
+			if tc.limit == 0 ***REMOVED***
+				wg.Add(tc.launches)
+			***REMOVED*** else if tc.launches < tc.limit ***REMOVED***
+				wg.Add(tc.launches)
+			***REMOVED*** else ***REMOVED***
+				wg.Add(tc.limit)
+			***REMOVED***
+
+			var counter uint32
+
+			for i := 0; i < tc.launches; i++ ***REMOVED***
+				go func() ***REMOVED***
+					l.Begin()
+					atomic.AddUint32(&counter, 1)
+					wg.Done()
+				***REMOVED***()
+			***REMOVED***
+			wg.Wait()
+			assert.Equal(t, uint32(tc.expMid), atomic.LoadUint32(&counter))
+
+			if tc.limit != 0 && tc.limit < tc.launches ***REMOVED***
+				wg.Add(tc.launches - tc.limit)
+				for i := 0; i < tc.launches; i++ ***REMOVED***
+					l.End()
+				***REMOVED***
+				wg.Wait()
+				assert.Equal(t, uint32(tc.launches), atomic.LoadUint32(&counter))
+			***REMOVED***
+		***REMOVED***)
+	***REMOVED***
+***REMOVED***
+
 func TestMultiSlotLimiter(t *testing.T) ***REMOVED***
+	t.Parallel()
 	t.Run("0", func(t *testing.T) ***REMOVED***
 		l := NewMultiSlotLimiter(0)
 		assert.Nil(t, l.Slot("test"))
@@ -47,5 +111,27 @@ func TestMultiSlotLimiter(t *testing.T) ***REMOVED***
 		l := NewMultiSlotLimiter(1)
 		assert.Equal(t, l.Slot("test"), l.Slot("test"))
 		assert.NotNil(t, l.Slot("test"))
+	***REMOVED***)
+	t.Run("2", func(t *testing.T) ***REMOVED***
+		l := NewMultiSlotLimiter(1)
+		wg := sync.WaitGroup***REMOVED******REMOVED***
+		wg.Add(2)
+
+		var s1, s2 SlotLimiter
+		go func() ***REMOVED***
+			s1 = l.Slot("ctest")
+			wg.Done()
+		***REMOVED***()
+		go func() ***REMOVED***
+			s2 = l.Slot("ctest")
+			wg.Done()
+		***REMOVED***()
+		wg.Wait()
+
+		assert.NotNil(t, s1)
+		assert.Equal(t, s1, s2)
+		assert.Equal(t, s1, l.Slot("ctest"))
+		assert.NotEqual(t, s1, l.Slot("dtest"))
+		assert.NotNil(t, l.Slot("dtest"))
 	***REMOVED***)
 ***REMOVED***
