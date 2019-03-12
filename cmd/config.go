8467648b680fe55cb58fd57b37ad9ae095b838e4
Nedyalko Andreev <n@andreev.sh>
2019-03-12 15:59:56 +0200

    Silence a linter warning... for now!

--- cmd/config.go
+++ cmd/config.go
@@ -305,6 +305,8 @@ func getConsolidatedConfig(fs afero.Fs, cliConf Config, runner lib.Runner) (conf
 	return buildExecutionConfig(conf)
 ***REMOVED***
 
+//TODO: remove ↓
+//nolint:unparam
 func validateConfig(conf Config) error ***REMOVED***
 	errList := conf.Validate()
 	if len(errList) == 0 ***REMOVED***
