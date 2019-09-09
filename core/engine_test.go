1fdaa410f7aa9832a8aea86ab8711d01a92d00ae
Ivan Mirić <ivan@loadimpact.com>
2019-09-09 18:23:22 +0200

    fix(core,test): update WebSocket URLs

--- core/engine_test.go
+++ core/engine_test.go
@@ -540,7 +540,7 @@ func TestSentReceivedMetrics(t *testing.T) ***REMOVED***
 		***REMOVED***tr(`import ws from "k6/ws";
 			let data = "0123456789".repeat(100);
 			export default function() ***REMOVED***
-				ws.connect("ws://HTTPBIN_IP:HTTPBIN_PORT/ws-echo", null, function (socket) ***REMOVED***
+				ws.connect("WSBIN_URL/ws-echo", null, function (socket) ***REMOVED***
 					socket.on('open', function open() ***REMOVED***
 						socket.send(data);
 					***REMOVED***);
@@ -667,7 +667,7 @@ func TestRunTags(t *testing.T) ***REMOVED***
 			***REMOVED***)
 
 			group("websockets", function() ***REMOVED***
-				var response = ws.connect("wss://HTTPSBIN_IP:HTTPSBIN_PORT/ws-echo", params, function (socket) ***REMOVED***
+				var response = ws.connect("WSBIN_URL/ws-echo", params, function (socket) ***REMOVED***
 					socket.on('open', function open() ***REMOVED***
 						console.log('ws open and say hello');
 						socket.send("hello");
