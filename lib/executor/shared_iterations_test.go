5c91d49c376463cc8b31abeba4d4e9ed827bb2f9
Ivan Mirić <ivan@imiric.com>
2019-11-29 12:45:26 +0100

    Add SharedIterations.Run unit test

---
+++ lib/executor/shared_iterations_test.go
@@ -0,0 +1,38 @@
+package executor
+
+import (
+	"context"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	null "gopkg.in/guregu/null.v3"
+
+	"github.com/loadimpact/k6/lib/types"
+)
+
+func getTestSharedIterationsConfig() SharedIterationsConfig ***REMOVED***
+	return SharedIterationsConfig***REMOVED***
+		VUs:         null.IntFrom(10),
+		Iterations:  null.IntFrom(100),
+		MaxDuration: types.NullDurationFrom(5 * time.Second),
+	***REMOVED***
+***REMOVED***
+
+func TestSharedIterationsRun(t *testing.T) ***REMOVED***
+	t.Parallel()
+	var doneIters uint64
+	var ctx, cancel, executor, _ = setupExecutor(
+		t, getTestSharedIterationsConfig(),
+		simpleRunner(func(ctx context.Context) error ***REMOVED***
+			atomic.AddUint64(&doneIters, 1)
+			return nil
+		***REMOVED***),
+	)
+	defer cancel()
+	err := executor.Run(ctx, nil)
+	require.NoError(t, err)
+	assert.Equal(t, uint64(100), doneIters)
+***REMOVED***
