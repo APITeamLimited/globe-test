d41def9bfcf26ceb64349216b2631d5b90781a6d
Mihail Stoykov <M.Stoikov@gmail.com>
2020-06-12 12:45:21 +0300

    fix js package http benchmark and add one with base compatibility-mode
    name                time/op
    HTTPRequests-8       668µs ± 2%
    HTTPRequestsBase-8   664µs ± 3%
    
    name                alloc/op
    HTTPRequests-8      27.6kB ± 0%
    HTTPRequestsBase-8  27.6kB ± 0%
    
    name                allocs/op
    HTTPRequests-8         365 ± 0%
    HTTPRequestsBase-8     365 ± 0%
    
    as can be seen the diffence is negligable and within the margin of error

--- js/http_bench_test.go
+++ js/http_bench_test.go
@@ -25,6 +25,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"gopkg.in/guregu/null.v3"
 
 	"github.com/loadimpact/k6/lib"
@@ -44,16 +45,61 @@ func BenchmarkHTTPRequests(b *testing.B) ***REMOVED***
 				let res = http.get(url + "/cookies/set?k2=v2&k1=v1");
 				if (res.status != 200) ***REMOVED*** throw new Error("wrong status: " + res.status) ***REMOVED***
 			***REMOVED***
-		`))
+		`), lib.RuntimeOptions***REMOVED***CompatibilityMode: null.StringFrom("extended")***REMOVED***)
 	if !assert.NoError(b, err) ***REMOVED***
 		return
 	***REMOVED***
-	r.SetOptions(lib.Options***REMOVED***
+	err = r.SetOptions(lib.Options***REMOVED***
 		Throw:          null.BoolFrom(true),
 		MaxRedirects:   null.IntFrom(10),
 		Hosts:          tb.Dialer.Hosts,
 		NoCookiesReset: null.BoolFrom(true),
 	***REMOVED***)
+	require.NoError(b, err)
+
+	var ch = make(chan stats.SampleContainer, 100)
+	go func() ***REMOVED*** // read the channel so it doesn't block
+		for ***REMOVED***
+			<-ch
+		***REMOVED***
+	***REMOVED***()
+	initVU, err := r.NewVU(1, ch)
+	if !assert.NoError(b, err) ***REMOVED***
+		return
+	***REMOVED***
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	vu := initVU.Activate(&lib.VUActivationParams***REMOVED***RunContext: ctx***REMOVED***)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ ***REMOVED***
+		err = vu.RunOnce()
+		assert.NoError(b, err)
+	***REMOVED***
+***REMOVED***
+
+func BenchmarkHTTPRequestsBase(b *testing.B) ***REMOVED***
+	b.StopTimer()
+	tb := httpmultibin.NewHTTPMultiBin(b)
+	defer tb.Cleanup()
+
+	r, err := getSimpleRunner("/script.js", tb.Replacer.Replace(`
+			var http = require("k6/http");
+			exports.default = function() ***REMOVED***
+				var url = "HTTPBIN_URL";
+				var res = http.get(url + "/cookies/set?k2=v2&k1=v1");
+				if (res.status != 200) ***REMOVED*** throw new Error("wrong status: " + res.status) ***REMOVED***
+			***REMOVED***
+		`))
+	if !assert.NoError(b, err) ***REMOVED***
+		return
+	***REMOVED***
+	err = r.SetOptions(lib.Options***REMOVED***
+		Throw:          null.BoolFrom(true),
+		MaxRedirects:   null.IntFrom(10),
+		Hosts:          tb.Dialer.Hosts,
+		NoCookiesReset: null.BoolFrom(true),
+	***REMOVED***)
+	require.NoError(b, err)
 
 	var ch = make(chan stats.SampleContainer, 100)
 	go func() ***REMOVED*** // read the channel so it doesn't block
