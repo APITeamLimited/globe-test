82393a6a5ab2f1127f4cb9401b7a46a83832a484
Mihail Stoykov <M.Stoikov@gmail.com>
2020-08-14 19:01:54 +0300

    Don't add another label just to count dropped messages

--- log/loki.go
+++ log/loki.go
@@ -296,15 +296,14 @@ func (h *lokiHook) createPushMessage(msgs []tmpMsg, cutOffIndex, dropped int) *l
 	if dropped != 0 ***REMOVED***
 		labels := make(map[string]string, 2+len(h.labels))
 		labels["level"] = logrus.WarnLevel.String()
-		labels["dropped"] = strconv.Itoa(dropped)
 		for _, params := range h.labels ***REMOVED***
 			labels[params[0]] = params[1]
 		***REMOVED***
 
 		msg := tmpMsg***REMOVED***
 			labels: labels,
-			msg: fmt.Sprintf("k6 dropped some log messages because they were above the limit of %d/%s",
-				h.limit, h.pushPeriod),
+			msg: fmt.Sprintf("k6 dropped some %d messages because they were above the limit of %d/%s",
+				dropped, h.limit, h.pushPeriod),
 			t: msgs[cutOffIndex-1].t,
 		***REMOVED***
 		pushMsg.add(msg)
