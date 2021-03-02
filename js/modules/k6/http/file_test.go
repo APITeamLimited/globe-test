a-cordier/k6

Per VU module instance support for the http module
part of #1858

js/modules/k6/http/file_test.go
@@ -80,7 +80,7 @@ func TestHTTPFile(t *testing.T) {
 					assert.Equal(t, tc.expErr, fmt.Sprintf("%s", val["value"]))
 				}()
 			}
-			h := New()
+			h := new(GlobalHTTP).NewVUModule().(*HTTP)
 			ctx := common.WithRuntime(context.Background(), rt)
 			out := h.File(ctx, tc.input, tc.args...)
 			assert.Equal(t, tc.expected, out)

js/modules/k6/http/http_test.go
@@ -34,7 +34,7 @@ import (
 func TestTagURL(t *testing.T) {
 	rt := goja.New()
 	rt.SetFieldNameMapper(common.FieldNameMapper{})
-	rt.Set("http", common.Bind(rt, New(), nil))
+	rt.Set("http", common.Bind(rt, new(GlobalHTTP).NewVUModule(), nil))
 
 	testdata := map[string]struct{ u, n string }{
 		`http://localhost/anything/`:               {"http://localhost/anything/", "http://localhost/anything/"},

js/modules/k6/http/request_test.go
@@ -169,7 +169,7 @@ func newRuntime(
 	ctx := new(context.Context)
 	*ctx = lib.WithState(tb.Context, state)
 	*ctx = common.WithRuntime(*ctx, rt)
-	rt.Set("http", common.Bind(rt, New(), ctx))
+	rt.Set("http", common.Bind(rt, new(GlobalHTTP).NewVUModule(), ctx))
 
 	return tb, state, samples, rt, ctx
 }

APITeamLimited/globe-test

js/modules/k6/http/file_test.go
@@ -80,7 +80,7 @@ func TestHTTPFile(t *testing.T) ***REMOVED***
 					assert.Equal(t, tc.expErr, fmt.Sprintf("%s", val["value"]))
 				***REMOVED***()
 			***REMOVED***
-			h := New()
+			h := new(GlobalHTTP).NewModuleInstancePerVU().(*HTTP)
 			ctx := common.WithRuntime(context.Background(), rt)
 			out := h.File(ctx, tc.input, tc.args...)
 			assert.Equal(t, tc.expected, out)

js/modules/k6/http/http_test.go
@@ -34,7 +34,7 @@ import (
 func TestTagURL(t *testing.T) ***REMOVED***
 	rt := goja.New()
 	rt.SetFieldNameMapper(common.FieldNameMapper***REMOVED******REMOVED***)
-	rt.Set("http", common.Bind(rt, New(), nil))
+	rt.Set("http", common.Bind(rt, new(GlobalHTTP).NewModuleInstancePerVU(), nil))
 
 	testdata := map[string]struct***REMOVED*** u, n string ***REMOVED******REMOVED***
 		`http://localhost/anything/`:               ***REMOVED***"http://localhost/anything/", "http://localhost/anything/"***REMOVED***,

js/modules/k6/http/request_test.go
@@ -169,7 +169,7 @@ func newRuntime(
 	ctx := new(context.Context)
 	*ctx = lib.WithState(tb.Context, state)
 	*ctx = common.WithRuntime(*ctx, rt)
-	rt.Set("http", common.Bind(rt, New(), ctx))
+	rt.Set("http", common.Bind(rt, new(GlobalHTTP).NewModuleInstancePerVU(), ctx))
 
 	return tb, state, samples, rt, ctx
 ***REMOVED***