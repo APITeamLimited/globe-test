aa7bf8cc61de1ce0706bf3fc879e9588188834ce
Ivan Mirić <ivan@loadimpact.com>
2021-03-22 15:48:25 +0100

    Sync remote to local with s3cmd

--- packaging/bin/create-msi-repo.sh
+++ packaging/bin/create-msi-repo.sh
@@ -12,20 +12,13 @@ PKGDIR="$***REMOVED***1?$***REMOVED***_usage***REMOVED******REMOVED***"  # The d
 REPODIR="$***REMOVED***2?$***REMOVED***_usage***REMOVED******REMOVED***" # The package repository working directory
 S3PATH="$***REMOVED***3-$***REMOVED***_s3bucket***REMOVED******REMOVED***/msi"
 
-# TODO: Replace with CDN URL
-#repobaseurl="https://dl.k6.io/msi"
-repobaseurl="http://test-dl-k6-io.s3-website.eu-north-1.amazonaws.com/msi"
-
 # TODO: Remove old package versions?
 # Something like: https://github.com/kopia/kopia/blob/master/tools/apt-publish.sh#L23-L25
 
-mkdir -p "$REPODIR" && cd "$_"
+mkdir -p "$REPODIR"
 
-# Download existing packages via the CDN to avoid S3 egress costs.
-# For MSIs this is just needed to generate the index correctly.
-# TODO: Also check their hashes? Or just sync them with s3cmd which does MD5 checks...
-files=$(s3cmd ls "s3://$***REMOVED***S3PATH***REMOVED***/" | ***REMOVED*** grep -oP "(?<=/$***REMOVED***S3PATH***REMOVED***/).*\.msi" || true; ***REMOVED***)
-# curl supports parallel downloads with the -Z option since v7.68.0, but
-# unfortunately Debian carries an older version, hence xargs.
-echo "$files" | xargs -r -I***REMOVED******REMOVED*** -n1 -P"$(nproc)" curl -fsSLOR "$repobaseurl/***REMOVED******REMOVED***"
+# Download existing packages
+s3cmd sync --exclude='*' --include='*.msi' "s3://$***REMOVED***S3PATH***REMOVED***/" "$REPODIR/"
+
+# Copy the new packages in
 find "$PKGDIR" -name "*.msi" -type f -print0 | xargs -r0 cp -t "$REPODIR"
--- packaging/bin/create-rpm-repo.sh
+++ packaging/bin/create-rpm-repo.sh
@@ -18,9 +18,6 @@ REPODIR="$***REMOVED***2?$***REMOVED***_usage***REMOVED******REMOVED***" # The p
 S3PATH="$***REMOVED***3-$***REMOVED***_s3bucket***REMOVED******REMOVED***/rpm"
 
 architectures="x86_64"
-# TODO: Replace with CDN URL
-#repobaseurl="https://dl.k6.io/rpm"
-repobaseurl="http://test-dl-k6-io.s3-website.eu-north-1.amazonaws.com/rpm"
 
 # TODO: Remove old package versions?
 # Something like: https://github.com/kopia/kopia/blob/master/tools/apt-publish.sh#L23-L25
@@ -29,12 +26,9 @@ mkdir -p "$REPODIR" && cd "$_"
 
 for arch in $architectures; do
   mkdir -p "$arch" && cd "$_"
-  # Download existing packages via the CDN to avoid S3 egress costs.
-  # TODO: Also check their hashes? Or just sync them with s3cmd which does MD5 checks...
-  files=$(s3cmd ls "s3://$***REMOVED***S3PATH***REMOVED***/$***REMOVED***arch***REMOVED***/" | ***REMOVED*** grep -oP "(?<=/$***REMOVED***S3PATH***REMOVED***/).*\.rpm" || true; ***REMOVED***)
-  # curl supports parallel downloads with the -Z option since v7.68.0, but
-  # unfortunately Debian carries an older version, hence xargs.
-  echo "$files" | xargs -r -I***REMOVED******REMOVED*** -n1 -P"$(nproc)" curl -fsSLOR "$repobaseurl/***REMOVED******REMOVED***"
+
+  # Download existing packages
+  s3cmd sync --exclude='*' --include='*.rpm' "s3://$***REMOVED***S3PATH***REMOVED***/$***REMOVED***arch***REMOVED***/" ./
 
   # Copy the new packages in and generate signatures
   # FIXME: The architecture naming used by yum docs and in public RPM repos is
