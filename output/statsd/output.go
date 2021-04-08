0a6e4e8ad217664809bbb53c087198930572baf6
Mihail Stoykov <M.Stoikov@gmail.com>
2021-04-08 14:29:41 +0300

    Rewrite statsd collector as output
    Also, deprecates the datadog one where instead now the
    statsd output should be used with configuring
    K6_STATSD_ENABLE_TAGS=true .
    
    The tag format k6 support is supported by (at least) Datadog,
    New Relic and statsd v0.9.0.
    
    Additionally, a new K6_STATSD_TAG_BLOCKLIST was added analogous to the
    K6_DATADOG_TAG_BLACKLIST.

---
+++ output/statsd/output.go
@@ -0,0 +1,185 @@
+/*
+ *
+ * k6 - a next-generation load testing tool
+ * Copyright (C) 2019 Load Impact
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package statsd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/DataDog/datadog-go/statsd"
+	"github.com/sirupsen/logrus"
+
+	"github.com/loadimpact/k6/output"
+	"github.com/loadimpact/k6/stats"
+)
+
+// New creates a new statsd connector client
+func New(params output.Params) (output.Output, error) ***REMOVED***
+	return newOutput(params)
+***REMOVED***
+
+func newOutput(params output.Params) (*Output, error) ***REMOVED***
+	conf, err := getConsolidatedConfig(params.JSONConfig, params.Environment, params.ConfigArgument)
+	if err != nil ***REMOVED***
+		return nil, err
+	***REMOVED***
+	logger := params.Logger.WithFields(logrus.Fields***REMOVED***"output": "statsd"***REMOVED***)
+
+	return &Output***REMOVED***
+		config: conf,
+		logger: logger,
+	***REMOVED***, nil
+***REMOVED***
+
+var _ output.Output = &Output***REMOVED******REMOVED***
+
+// Output sends result data to statsd daemons with the ability to send to datadog as well
+type Output struct ***REMOVED***
+	output.SampleBuffer
+
+	periodicFlusher *output.PeriodicFlusher
+
+	config config
+
+	logger logrus.FieldLogger
+	client *statsd.Client
+***REMOVED***
+
+func (o *Output) dispatch(entry stats.Sample) error ***REMOVED***
+	var tagList []string
+	if o.config.EnableTags.Bool ***REMOVED***
+		tagList = processTags(o.config.TagBlocklist, entry.Tags.CloneTags())
+	***REMOVED***
+
+	switch entry.Metric.Type ***REMOVED***
+	case stats.Counter:
+		return o.client.Count(entry.Metric.Name, int64(entry.Value), tagList, 1)
+	case stats.Trend:
+		return o.client.TimeInMilliseconds(entry.Metric.Name, entry.Value, tagList, 1)
+	case stats.Gauge:
+		return o.client.Gauge(entry.Metric.Name, entry.Value, tagList, 1)
+	case stats.Rate:
+		if check, ok := entry.Tags.Get("check"); ok ***REMOVED***
+			return o.client.Count(
+				checkToString(check, entry.Value),
+				1,
+				tagList,
+				1,
+			)
+		***REMOVED***
+		return o.client.Count(entry.Metric.Name, int64(entry.Value), tagList, 1)
+	default:
+		return fmt.Errorf("unsupported metric type %s", entry.Metric.Type)
+	***REMOVED***
+***REMOVED***
+
+func checkToString(check string, value float64) string ***REMOVED***
+	label := "pass"
+	if value == 0 ***REMOVED***
+		label = "fail"
+	***REMOVED***
+	return "check." + check + "." + label
+***REMOVED***
+
+// Description returns a human-readable description of the output.
+func (o *Output) Description() string ***REMOVED***
+	return fmt.Sprintf("statsd (%s)", o.config.Addr.String)
+***REMOVED***
+
+// Start tries to open a connection to specified statsd service and starts the goroutine for
+// metric flushing.
+func (o *Output) Start() error ***REMOVED***
+	o.logger.Debug("Starting...")
+
+	var err error
+	if address := o.config.Addr.String; address == "" ***REMOVED***
+		err = fmt.Errorf(
+			"connection string is invalid. Received: \"%+s\"",
+			address,
+		)
+		o.logger.Error(err)
+
+		return err
+	***REMOVED***
+
+	o.client, err = statsd.NewBuffered(o.config.Addr.String, int(o.config.BufferSize.Int64))
+
+	if err != nil ***REMOVED***
+		o.logger.Errorf("Couldn't make buffered client, %s", err)
+		return err
+	***REMOVED***
+
+	if namespace := o.config.Namespace.String; namespace != "" ***REMOVED***
+		o.client.Namespace = namespace
+	***REMOVED***
+
+	pf, err := output.NewPeriodicFlusher(time.Duration(o.config.PushInterval.Duration), o.flushMetrics)
+	if err != nil ***REMOVED***
+		return err
+	***REMOVED***
+	o.logger.Debug("Started!")
+	o.periodicFlusher = pf
+
+	return nil
+***REMOVED***
+
+// Stop flushes any remaining metrics and stops the goroutine.
+func (o *Output) Stop() error ***REMOVED***
+	o.logger.Debug("Stopping...")
+	defer o.logger.Debug("Stopped!")
+	o.periodicFlusher.Stop()
+	return o.client.Close()
+***REMOVED***
+
+func (o *Output) flushMetrics() ***REMOVED***
+	samples := o.GetBufferedSamples()
+	start := time.Now()
+	var count int
+	var errorCount int
+	for _, sc := range samples ***REMOVED***
+		samples := sc.GetSamples()
+		count += len(samples)
+		o.logger.
+			WithField("samples", len(samples)).
+			Debug("Pushing metrics to server")
+
+		for _, entry := range samples ***REMOVED***
+			if err := o.dispatch(entry); err != nil ***REMOVED***
+				// No need to return error if just one metric didn't go through
+				o.logger.WithError(err).Debugf("Error while sending metric %s", entry.Metric.Name)
+				errorCount++
+			***REMOVED***
+		***REMOVED***
+	***REMOVED***
+
+	if count > 0 ***REMOVED***
+		if errorCount != 0 ***REMOVED***
+			o.logger.Warnf("Couldn't send %d out of %d metrics. Enable debug logging to see individual errors",
+				errorCount, count)
+		***REMOVED***
+		if err := o.client.Flush(); err != nil ***REMOVED***
+			o.logger.
+				WithError(err).
+				Error("Couldn't flush a batch")
+		***REMOVED***
+		o.logger.WithField("t", time.Since(start)).WithField("count", count).Debug("Wrote metrics to statsd")
+	***REMOVED***
+***REMOVED***
---
+++ output/statsd/output_test.go
@@ -0,0 +1,141 @@
+/*
+ *
+ * k6 - a next-generation load testing tool
+ * Copyright (C) 2019 Load Impact
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package statsd
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/guregu/null.v3"
+
+	"github.com/loadimpact/k6/lib/testutils"
+	"github.com/loadimpact/k6/lib/types"
+	"github.com/loadimpact/k6/output"
+	"github.com/loadimpact/k6/stats"
+)
+
+func getOutput(
+	logger logrus.FieldLogger, addr, namespace null.String, bufferSize null.Int, pushInterval types.NullDuration,
+) (*Output, error) ***REMOVED***
+	return newOutput(
+		output.Params***REMOVED***
+			Logger: logger,
+			JSONConfig: json.RawMessage(fmt.Sprintf(`***REMOVED***
+			"addr": "%s",
+			"namespace": "%s",
+			"bufferSize": %d,
+			"pushInterval": "%s"
+		***REMOVED***`, addr.String, namespace.String, bufferSize.Int64, pushInterval.Duration.String())),
+		***REMOVED***)
+***REMOVED***
+
+func TestStatsdOutput(t *testing.T) ***REMOVED***
+	t.Parallel()
+	baseTest(t, getOutput,
+		func(t *testing.T, _ []stats.SampleContainer, expectedOutput, output string) ***REMOVED***
+			assert.Equal(t, expectedOutput, output)
+		***REMOVED***)
+***REMOVED***
+
+func TestStatsdEnabledTags(t *testing.T) ***REMOVED***
+	t.Parallel()
+	tagMap := stats.TagSet***REMOVED***"tag1": true, "tag2": true***REMOVED***
+
+	baseTest(t, func(
+		logger logrus.FieldLogger, addr, namespace null.String, bufferSize null.Int, pushInterval types.NullDuration,
+	) (*Output, error) ***REMOVED***
+		return newOutput(
+			output.Params***REMOVED***
+				Logger: logger,
+				JSONConfig: json.RawMessage(fmt.Sprintf(`***REMOVED***
+			"addr": "%s",
+			"namespace": "%s",
+			"bufferSize": %d,
+			"pushInterval": "%s",
+			"tagBlocklist": ["tag1", "tag2"],
+			"enableTags": true
+		***REMOVED***`, addr.String, namespace.String, bufferSize.Int64, pushInterval.Duration.String())),
+			***REMOVED***)
+	***REMOVED***, func(t *testing.T, containers []stats.SampleContainer, expectedOutput, output string) ***REMOVED***
+		outputLines := strings.Split(output, "\n")
+		expectedOutputLines := strings.Split(expectedOutput, "\n")
+		var lines int
+
+		for i, container := range containers ***REMOVED***
+			for j, sample := range container.GetSamples() ***REMOVED***
+				lines++
+				var (
+					expectedTagList    = processTags(tagMap, sample.GetTags().CloneTags())
+					expectedOutputLine = expectedOutputLines[i*j+i]
+					outputLine         = outputLines[i*j+i]
+					outputWithoutTags  = outputLine
+					outputTagList      = []string***REMOVED******REMOVED***
+					tagSplit           = strings.LastIndex(outputLine, "|#")
+				)
+				if tagSplit != -1 ***REMOVED***
+					outputWithoutTags = outputLine[:tagSplit]
+					outputTagList = strings.Split(outputLine[tagSplit+len("|#"):], ",")
+				***REMOVED***
+				require.Equal(t, expectedOutputLine, outputWithoutTags)
+				require.ElementsMatch(t, expectedTagList, outputTagList)
+			***REMOVED***
+		***REMOVED***
+		require.Equal(t, lines, len(outputLines))
+	***REMOVED***)
+***REMOVED***
+
+func TestInitWithoutAddressErrors(t *testing.T) ***REMOVED***
+	t.Parallel()
+	c := &Output***REMOVED***
+		config: config***REMOVED******REMOVED***,
+		logger: testutils.NewLogger(t),
+	***REMOVED***
+	err := c.Start()
+	require.Error(t, err)
+***REMOVED***
+
+func TestInitWithBogusAddressErrors(t *testing.T) ***REMOVED***
+	t.Parallel()
+	c := &Output***REMOVED***
+		config: config***REMOVED***
+			Addr: null.StringFrom("localhost:90000"),
+		***REMOVED***,
+		logger: testutils.NewLogger(t),
+	***REMOVED***
+	err := c.Start()
+	require.Error(t, err)
+***REMOVED***
+
+func TestLinkReturnAddress(t *testing.T) ***REMOVED***
+	t.Parallel()
+	bogusValue := "bogus value"
+	c := &Output***REMOVED***
+		config: config***REMOVED***
+			Addr: null.StringFrom(bogusValue),
+		***REMOVED***,
+	***REMOVED***
+	require.Equal(t, fmt.Sprintf("statsd (%s)", bogusValue), c.Description())
+***REMOVED***
