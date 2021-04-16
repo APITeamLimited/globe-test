d56b8a3a826ab68fd9b83d6b9b7db162bb648d43
Nedyalko Andreev <n@andreev.sh>
2021-04-16 22:21:11 +0300

    Do not use global variables and state to colorize some UI elements
    We still use the noColor and stdoutTTY global variables, but we explicitly pass them as function arguments, so it's clear which parts of the code depend on them when we eventually fully refactor them into locals.

--- cmd/ui.go
+++ cmd/ui.go
@@ -32,12 +32,13 @@ import (
 	"time"
 	"unicode/utf8"
 
+	"github.com/fatih/color"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/crypto/ssh/terminal"
 
 	"go.k6.io/k6/lib"
+	"go.k6.io/k6/lib/consts"
 	"go.k6.io/k6/output"
-	"go.k6.io/k6/ui"
 	"go.k6.io/k6/ui/pb"
 )
 
@@ -81,6 +82,26 @@ func (w *consoleWriter) Write(p []byte) (n int, err error) ***REMOVED***
 	return origLen, err
 ***REMOVED***
 
+// getColor returns the requested color, or an uncolored object, depending on
+// the value of noColor. The explicit EnableColor() and DisableColor() are
+// needed because the library checks os.Stdout itself otherwise...
+func getColor(noColor bool, attributes ...color.Attribute) *color.Color ***REMOVED***
+	if noColor ***REMOVED***
+		c := color.New()
+		c.DisableColor()
+		return c
+	***REMOVED***
+
+	c := color.New(attributes...)
+	c.EnableColor()
+	return c
+***REMOVED***
+
+func getBanner(noColor bool) string ***REMOVED***
+	c := getColor(noColor, color.FgCyan)
+	return c.Sprint(consts.Banner())
+***REMOVED***
+
 func printBar(bar *pb.ProgressBar) ***REMOVED***
 	if quiet ***REMOVED***
 		return
@@ -113,10 +134,12 @@ func modifyAndPrintBar(bar *pb.ProgressBar, options ...pb.ProgressBarOption) ***
 // TODO: Clean this up as part of #1499 or #1427
 func printExecutionDescription(
 	execution, filename, outputOverride string, conf Config, et *lib.ExecutionTuple,
-	execPlan []lib.ExecutionStep, outputs []output.Output,
+	execPlan []lib.ExecutionStep, outputs []output.Output, noColor bool,
 ) ***REMOVED***
-	fprintf(stdout, "  execution: %s\n", ui.ValueColor.Sprint(execution))
-	fprintf(stdout, "     script: %s\n", ui.ValueColor.Sprint(filename))
+	valueColor := getColor(noColor, color.FgCyan)
+
+	fprintf(stdout, "  execution: %s\n", valueColor.Sprint(execution))
+	fprintf(stdout, "     script: %s\n", valueColor.Sprint(filename))
 
 	var outputDescriptions []string
 	switch ***REMOVED***
@@ -130,7 +153,7 @@ func printExecutionDescription(
 		***REMOVED***
 	***REMOVED***
 
-	fprintf(stdout, "     output: %s\n", ui.ValueColor.Sprint(strings.Join(outputDescriptions, ", ")))
+	fprintf(stdout, "     output: %s\n", valueColor.Sprint(strings.Join(outputDescriptions, ", ")))
 	fprintf(stdout, "\n")
 
 	maxDuration, _ := lib.GetEndOffset(execPlan)
@@ -141,7 +164,7 @@ func printExecutionDescription(
 		scenarioDesc = fmt.Sprintf("%d scenarios", len(executorConfigs))
 	***REMOVED***
 
-	fprintf(stdout, "  scenarios: %s\n", ui.ValueColor.Sprintf(
+	fprintf(stdout, "  scenarios: %s\n", valueColor.Sprintf(
 		"(%.2f%%) %s, %d max VUs, %s max duration (incl. graceful stop):",
 		conf.ExecutionSegment.FloatLength()*100, scenarioDesc,
 		lib.GetMaxPossibleVUs(execPlan), maxDuration.Round(100*time.Millisecond)),
