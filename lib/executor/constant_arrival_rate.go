grafana/k6

Accept ExecutionTuple in SegmentedIndex constructor
Resolves https://github.com/k6io/k6/pull/1863#discussion_r649899233

lib/executor/constant_arrival_rate.go
@@ -196,8 +196,7 @@ func (car *ConstantArrivalRate) Init(ctx context.Context) error {
 	// with no work, as determined by their config's HasWork() method.
 	et, err := car.BaseExecutor.executionState.ExecutionTuple.GetNewExecutionTupleFromValue(car.config.MaxVUs.Int64)
 	car.et = et
-	start, offsets, lcd := et.GetStripedOffsets()
-	car.iterationSegIndex = lib.NewSegmentedIndex(start, lcd, offsets)
+	car.iterationSegIndex = lib.NewSegmentedIndex(et)
 
 	return err
 }

lib/executor/ramping_arrival_rate.go
@@ -187,8 +187,7 @@ func (varr *RampingArrivalRate) Init(ctx context.Context) error {
 	// with no work, as determined by their config's HasWork() method.
 	et, err := varr.BaseExecutor.executionState.ExecutionTuple.GetNewExecutionTupleFromValue(varr.config.MaxVUs.Int64)
 	varr.et = et
-	start, offsets, lcd := et.GetStripedOffsets()
-	varr.iterationSegIndex = lib.NewSegmentedIndex(start, lcd, offsets)
+	varr.iterationSegIndex = lib.NewSegmentedIndex(et)
 
 	return err //nolint: wrapcheck
 }

lib/executor/shared_iterations.go
@@ -176,8 +176,7 @@ func (si *SharedIterations) Init(ctx context.Context) error {
 	// with no work, as determined by their config's HasWork() method.
 	et, err := si.BaseExecutor.executionState.ExecutionTuple.GetNewExecutionTupleFromValue(si.config.VUs.Int64)
 	si.et = et
-	start, offsets, lcd := et.GetStripedOffsets()
-	si.iterationSegIndex = lib.NewSegmentedIndex(start, lcd, offsets)
+	si.iterationSegIndex = lib.NewSegmentedIndex(et)
 
 	return err
 }

APITeamLimited/globe-test

lib/executor/constant_arrival_rate.go
@@ -196,8 +196,7 @@ func (car *ConstantArrivalRate) Init(ctx context.Context) error ***REMOVED***
 	// with no work, as determined by their config's HasWork() method.
 	et, err := car.BaseExecutor.executionState.ExecutionTuple.GetNewExecutionTupleFromValue(car.config.MaxVUs.Int64)
 	car.et = et
-	start, offsets, lcd := et.GetStripedOffsets()
-	car.iterSegIndex = lib.NewSegmentedIndex(start, lcd, offsets)
+	car.iterSegIndex = lib.NewSegmentedIndex(et)
 
 	return err
 ***REMOVED***

lib/executor/ramping_arrival_rate.go
@@ -187,8 +187,7 @@ func (varr *RampingArrivalRate) Init(ctx context.Context) error ***REMOVED***
 	// with no work, as determined by their config's HasWork() method.
 	et, err := varr.BaseExecutor.executionState.ExecutionTuple.GetNewExecutionTupleFromValue(varr.config.MaxVUs.Int64)
 	varr.et = et
-	start, offsets, lcd := et.GetStripedOffsets()
-	varr.iterSegIndex = lib.NewSegmentedIndex(start, lcd, offsets)
+	varr.iterSegIndex = lib.NewSegmentedIndex(et)
 
 	return err //nolint: wrapcheck
 ***REMOVED***

lib/executor/shared_iterations.go
@@ -176,8 +176,7 @@ func (si *SharedIterations) Init(ctx context.Context) error ***REMOVED***
 	// with no work, as determined by their config's HasWork() method.
 	et, err := si.BaseExecutor.executionState.ExecutionTuple.GetNewExecutionTupleFromValue(si.config.VUs.Int64)
 	si.et = et
-	start, offsets, lcd := et.GetStripedOffsets()
-	si.iterSegIndex = lib.NewSegmentedIndex(start, lcd, offsets)
+	si.iterSegIndex = lib.NewSegmentedIndex(et)
 
 	return err
 ***REMOVED***