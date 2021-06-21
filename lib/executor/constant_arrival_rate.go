grafana/k6

Use a single segmented index for local and global iterations (#2057)
* Use a single segmented index for local and global iterations

This removes the need for a lot of the additional synchronization.

This also makes it so there are global iterations for all executors. For
some of those it means that the global iteration will be *unique* among
instances but doesn't necessary mean that iterations with smaller
numbers *have* run or *will* run in the same test run.

This is somewhat like how it is for the executors for which we already
had it, there is nothing making certain that a shared-iterations
executor will not run out of time to run all the iterations on one of
the instances or a arrival-rate to not have to drop iterations due to
lack VUs to execute them.

* Update lib/executor/base_executor.go

Co-authored-by: Ivan Mirić <ivan@loadimpact.com>

* unexport and comment nextIterationCounters

* Remove synchronization in SegmetnedIndex

* Drop SegmentedIndexResult

* Fix race in TestSharedIterationsGlobalIters

There is nothing stopping 5 VUs starting a shared iterations at the same
time and one of them getting "slower" to the actual execution than it's
iteration counter suggests.

Co-authored-by: Ivan Mirić <ivan@loadimpact.com>

lib/executor/constant_arrival_rate.go
@@ -185,7 +185,6 @@ type ConstantArrivalRate struct {
 	*BaseExecutor
 	config ConstantArrivalRateConfig
 	et     *lib.ExecutionTuple
-	segIdx *lib.SegmentedIndex
 }
 
 // Make sure we implement the lib.Executor interface.
@@ -198,19 +197,9 @@ func (car *ConstantArrivalRate) Init(ctx context.Context) error {
 	et, err := car.BaseExecutor.executionState.ExecutionTuple.GetNewExecutionTupleFromValue(car.config.MaxVUs.Int64)
 	car.et = et
 	start, offsets, lcd := et.GetStripedOffsets()
-	car.segIdx = lib.NewSegmentedIndex(start, lcd, offsets)
+	car.iterationSegIndex = lib.NewSegmentedIndex(start, lcd, offsets)
 
 	return err
-}
-
-// getNextGlobalIter advances and returns the next global iteration number for
-// this executor, taking into account the configured execution segment.
-// Unlike the local iteration number returned by getNextLocalIter(), this
-// iteration number will be unique across k6 instances.
-func (car *ConstantArrivalRate) getNextGlobalIter() uint64 {
-	res := car.segIdx.Next()
-	// iterations are 0-based
-	return uint64(res.Unscaled - 1)
 }
 
 // Run executes a constant number of iterations per second.
@@ -293,15 +282,12 @@ func (car ConstantArrivalRate) Run(parentCtx context.Context, out chan<- stats.S
 		activeVUsWg.Done()
 	}
 
-	// Channel for synchronizing scenario-specific iteration increments
-	iterSync := make(chan struct{}, 1)
 	runIterationBasic := getIterationRunner(car.executionState, car.logger)
 	activateVU := func(initVU lib.InitializedVU) lib.ActiveVU {
 		activeVUsWg.Add(1)
 		activeVU := initVU.Activate(getVUActivationParams(
 			maxDurationCtx, car.config.BaseConfig, returnVU,
-			car.getNextLocalVUID, car.getNextLocalIter, car.getNextGlobalIter,
-			iterSync,
+			car.getNextLocalVUID, car.nextIterationCounters,
 		))
 		car.executionState.ModCurrentlyActiveVUsCount(+1)
 		atomic.AddUint64(&activeVUsCount, 1)

APITeamLimited/globe-test

lib/executor/constant_arrival_rate.go
@@ -185,7 +185,6 @@ type ConstantArrivalRate struct ***REMOVED***
 	*BaseExecutor
 	config ConstantArrivalRateConfig
 	et     *lib.ExecutionTuple
-	segIdx *lib.SegmentedIndex
 ***REMOVED***
 
 // Make sure we implement the lib.Executor interface.
@@ -198,19 +197,9 @@ func (car *ConstantArrivalRate) Init(ctx context.Context) error ***REMOVED***
 	et, err := car.BaseExecutor.executionState.ExecutionTuple.GetNewExecutionTupleFromValue(car.config.MaxVUs.Int64)
 	car.et = et
 	start, offsets, lcd := et.GetStripedOffsets()
-	car.segIdx = lib.NewSegmentedIndex(start, lcd, offsets)
+	car.iterSegIndex = lib.NewSegmentedIndex(start, lcd, offsets)
 
 	return err
-***REMOVED***
-
-// getNextGlobalIter advances and returns the next global iteration number for
-// this executor, taking into account the configured execution segment.
-// Unlike the local iteration number returned by getNextLocalIter(), this
-// iteration number will be unique across k6 instances.
-func (car *ConstantArrivalRate) getNextGlobalIter() uint64 ***REMOVED***
-	res := car.segIdx.Next()
-	// iterations are 0-based
-	return uint64(res.Unscaled - 1)
 ***REMOVED***
 
 // Run executes a constant number of iterations per second.
@@ -293,15 +282,12 @@ func (car ConstantArrivalRate) Run(parentCtx context.Context, out chan<- stats.S
 		activeVUsWg.Done()
 	***REMOVED***
 
-	// Channel for synchronizing scenario-specific iteration increments
-	iterSync := make(chan struct***REMOVED******REMOVED***, 1)
 	runIterationBasic := getIterationRunner(car.executionState, car.logger)
 	activateVU := func(initVU lib.InitializedVU) lib.ActiveVU ***REMOVED***
 		activeVUsWg.Add(1)
 		activeVU := initVU.Activate(getVUActivationParams(
 			maxDurationCtx, car.config.BaseConfig, returnVU,
-			car.getNextLocalVUID, car.getNextLocalIter, car.getNextGlobalIter,
-			iterSync,
+			car.getNextLocalVUID, car.nextIterationCounters,
 		))
 		car.executionState.ModCurrentlyActiveVUsCount(+1)
 		atomic.AddUint64(&activeVUsCount, 1)