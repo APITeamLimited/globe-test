grafana/k6

Add metrics registry

lib/executor/ramping_vus.go
@@ -31,6 +31,7 @@ import (
 	"gopkg.in/guregu/null.v3"
 
 	"go.k6.io/k6/lib"
+	"go.k6.io/k6/lib/metrics"
 	"go.k6.io/k6/lib/types"
 	"go.k6.io/k6/stats"
 	"go.k6.io/k6/ui/pb"
@@ -496,8 +497,10 @@ var _ lib.Executor = &RampingVUs{}
 // TODO: split up? since this does a ton of things, unfortunately I can't think
 // of a less complex way to implement it (besides the old "increment by 100ms
 // and see what happens)... :/ so maybe see how it can be split?
-// nolint:funlen,gocognit
-func (vlv RampingVUs) Run(parentCtx context.Context, out chan<- stats.SampleContainer) (err error) {
+// nolint:funlen,gocognit,cyclop
+func (vlv RampingVUs) Run(
+	parentCtx context.Context, out chan<- stats.SampleContainer, builtinMetrics *metrics.BuiltinMetrics,
+) (err error) {
 	rawExecutionSteps := vlv.config.getRawExecutionSteps(vlv.executionState.ExecutionTuple, true)
 	regularDuration, isFinal := lib.GetEndOffset(rawExecutionSteps)
 	if !isFinal {

APITeamLimited/globe-test

lib/executor/ramping_vus.go
@@ -31,6 +31,7 @@ import (
 	"gopkg.in/guregu/null.v3"
 
 	"go.k6.io/k6/lib"
+	"go.k6.io/k6/lib/metrics"
 	"go.k6.io/k6/lib/types"
 	"go.k6.io/k6/stats"
 	"go.k6.io/k6/ui/pb"
@@ -496,8 +497,10 @@ var _ lib.Executor = &RampingVUs***REMOVED******REMOVED***
 // TODO: split up? since this does a ton of things, unfortunately I can't think
 // of a less complex way to implement it (besides the old "increment by 100ms
 // and see what happens)... :/ so maybe see how it can be split?
-// nolint:funlen,gocognit
-func (vlv RampingVUs) Run(parentCtx context.Context, out chan<- stats.SampleContainer) (err error) ***REMOVED***
+// nolint:funlen,gocognit,cyclop
+func (vlv RampingVUs) Run(
+	parentCtx context.Context, out chan<- stats.SampleContainer, _ *metrics.BuiltinMetrics,
+) (err error) ***REMOVED***
 	rawExecutionSteps := vlv.config.getRawExecutionSteps(vlv.executionState.ExecutionTuple, true)
 	regularDuration, isFinal := lib.GetEndOffset(rawExecutionSteps)
 	if !isFinal ***REMOVED***