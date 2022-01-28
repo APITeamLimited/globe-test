Karitham/k6

Remove the last global variables in cmd
Still some global state dependency :(

cmd/cloud.go
@@ -74,7 +74,7 @@ This will execute the test on the k6 cloud service. Use "k6 login cloud" to auth
 				}
 			}
 			// TODO: disable in quiet mode?
-			_, _ = fmt.Fprintf(stdout, "\n%s\n\n", getBanner(fl.noColor || !stdoutTTY))
+			_, _ = fmt.Fprintf(fl.stdout, "\n%s\n\n", getBanner(fl.noColor || !fl.stdoutTTY))
 
 			progressBar := pb.New(
 				pb.WithConstLeft("Init"),
@@ -230,7 +230,7 @@ This will execute the test on the k6 cloud service. Use "k6 login cloud" to auth
 			executionPlan := derivedConf.Scenarios.GetFullExecutionRequirements(et)
 			printExecutionDescription(
 				"cloud", filename, testURL, derivedConf, et,
-				executionPlan, nil, fl.noColor || !stdoutTTY,
+				executionPlan, nil, fl.noColor || !fl.stdoutTTY, fl,
 			)
 
 			modifyAndPrintBar(
@@ -320,8 +320,8 @@ This will execute the test on the k6 cloud service. Use "k6 login cloud" to auth
 				return errext.WithExitCodeIfNone(errors.New("Test progress error"), exitcodes.CloudFailedToGetProgress)
 			}
 
-			valueColor := getColor(fl.noColor || !stdoutTTY, color.FgCyan)
-			fprintf(stdout, "     test status: %s\n", valueColor.Sprint(testProgress.RunStatusText))
+			valueColor := getColor(fl.noColor || !fl.stdoutTTY, color.FgCyan)
+			fprintf(fl.stdout, "     test status: %s\n", valueColor.Sprint(testProgress.RunStatusText))
 
 			if testProgress.ResultStatus == cloudapi.ResultStatusFailed {
 				// TODO: use different exit codes for failed thresholds vs failed test (e.g. aborted by system/limit)

cmd/pause.go
@@ -49,7 +49,7 @@ func getPauseCmd(ctx context.Context, fl *commandFlags) *cobra.Command {
 			if err != nil {
 				return err
 			}
-			return yamlPrint(stdout, status)
+			return yamlPrint(fl.stdout, status)
 		},
 	}
 	return pauseCmd

cmd/resume.go
@@ -50,7 +50,7 @@ func getResumeCmd(ctx context.Context, fl *commandFlags) *cobra.Command {
 				return err
 			}
 
-			return yamlPrint(stdout, status)
+			return yamlPrint(fl.stdout, status)
 		},
 	}
 	return resumeCmd

cmd/scale.go
@@ -54,7 +54,7 @@ func getScaleCmd(ctx context.Context, fl *commandFlags) *cobra.Command {
 				return err
 			}
 
-			return yamlPrint(stdout, status)
+			return yamlPrint(fl.stdout, status)
 		},
 	}
 

cmd/stats.go
@@ -46,7 +46,7 @@ func getStatsCmd(ctx context.Context, fl *commandFlags) *cobra.Command {
 				return err
 			}
 
-			return yamlPrint(stdout, metrics)
+			return yamlPrint(fl.stdout, metrics)
 		},
 	}
 	return statsCmd

cmd/status.go
@@ -46,7 +46,7 @@ func getStatusCmd(ctx context.Context, fl *commandFlags) *cobra.Command {
 				return err
 			}
 
-			return yamlPrint(stdout, status)
+			return yamlPrint(fl.stdout, status)
 		},
 	}
 	return statusCmd

cmd/ui.go
@@ -113,7 +113,7 @@ func printBar(bar *pb.ProgressBar, fl *commandFlags) {
 	// stateless... basically first render the left and right parts, so we know
 	// how long the longest line is, and how much space we have for the progress
 	widthDelta := -defaultTermWidth
-	if stdout.IsTTY {
+	if fl.stdout.IsTTY {
 		// If we're in a TTY, instead of printing the bar and going to the next
 		// line, erase everything till the end of the line and return to the
 		// start, so that the next print will overwrite the same line.
@@ -124,7 +124,7 @@ func printBar(bar *pb.ProgressBar, fl *commandFlags) {
 	}
 	rendered := bar.Render(0, widthDelta)
 	// Only output the left and middle part of the progress bar
-	fprintf(stdout, "%s%s", rendered.String(), end)
+	fprintf(fl.stdout, "%s%s", rendered.String(), end)
 }
 
 func modifyAndPrintBar(bar *pb.ProgressBar, fl *commandFlags, options ...pb.ProgressBarOption) {
@@ -136,12 +136,12 @@ func modifyAndPrintBar(bar *pb.ProgressBar, fl *commandFlags, options ...pb.Prog
 // TODO: Clean this up as part of #1499 or #1427
 func printExecutionDescription(
 	execution, filename, outputOverride string, conf Config, et *lib.ExecutionTuple,
-	execPlan []lib.ExecutionStep, outputs []output.Output, noColor bool,
+	execPlan []lib.ExecutionStep, outputs []output.Output, noColor bool, fl *commandFlags,
 ) {
 	valueColor := getColor(noColor, color.FgCyan)
 
-	fprintf(stdout, "  execution: %s\n", valueColor.Sprint(execution))
-	fprintf(stdout, "     script: %s\n", valueColor.Sprint(filename))
+	fprintf(fl.stdout, "  execution: %s\n", valueColor.Sprint(execution))
+	fprintf(fl.stdout, "     script: %s\n", valueColor.Sprint(filename))
 
 	var outputDescriptions []string
 	switch {
@@ -155,8 +155,8 @@ func printExecutionDescription(
 		}
 	}
 
-	fprintf(stdout, "     output: %s\n", valueColor.Sprint(strings.Join(outputDescriptions, ", ")))
-	fprintf(stdout, "\n")
+	fprintf(fl.stdout, "     output: %s\n", valueColor.Sprint(strings.Join(outputDescriptions, ", ")))
+	fprintf(fl.stdout, "\n")
 
 	maxDuration, _ := lib.GetEndOffset(execPlan)
 	executorConfigs := conf.Scenarios.GetSortedConfigs()
@@ -166,16 +166,16 @@ func printExecutionDescription(
 		scenarioDesc = fmt.Sprintf("%d scenarios", len(executorConfigs))
 	}
 
-	fprintf(stdout, "  scenarios: %s\n", valueColor.Sprintf(
+	fprintf(fl.stdout, "  scenarios: %s\n", valueColor.Sprintf(
 		"(%.2f%%) %s, %d max VUs, %s max duration (incl. graceful stop):",
 		conf.ExecutionSegment.FloatLength()*100, scenarioDesc,
 		lib.GetMaxPossibleVUs(execPlan), maxDuration.Round(100*time.Millisecond)),
 	)
 	for _, ec := range executorConfigs {
-		fprintf(stdout, "           * %s: %s\n",
+		fprintf(fl.stdout, "           * %s: %s\n",
 			ec.GetName(), ec.GetDescription(et))
 	}
-	fprintf(stdout, "\n")
+	fprintf(fl.stdout, "\n")
 }
 
 //nolint: funlen
@@ -279,7 +279,7 @@ func showProgress(ctx context.Context, pbs []*pb.ProgressBar, logger *logrus.Log
 
 	var errTermGetSize bool
 	termWidth := defaultTermWidth
-	if stdoutTTY {
+	if fl.stdoutTTY {
 		tw, _, err := term.GetSize(int(os.Stdout.Fd()))
 		if !(tw > 0) || err != nil {
 			errTermGetSize = true
@@ -304,14 +304,14 @@ func showProgress(ctx context.Context, pbs []*pb.ProgressBar, logger *logrus.Log
 
 	printProgressBars := func() {
 		progressBarsLastRenderLock.Lock()
-		_, _ = stdout.Writer.Write(progressBarsLastRender)
+		_, _ = fl.stdout.Writer.Write(progressBarsLastRender)
 		progressBarsLastRenderLock.Unlock()
 	}
 
 	var widthDelta int
 	// Default to responsive progress bars when in an interactive terminal
 	renderProgressBars := func(goBack bool) {
-		barText, longestLine := renderMultipleBars(stdoutTTY, goBack, maxLeft, termWidth, widthDelta, pbs, fl)
+		barText, longestLine := renderMultipleBars(fl.stdoutTTY, goBack, maxLeft, termWidth, widthDelta, pbs, fl)
 		widthDelta = termWidth - longestLine - termPadding
 		progressBarsLastRenderLock.Lock()
 		progressBarsLastRender = []byte(barText)
@@ -319,10 +319,10 @@ func showProgress(ctx context.Context, pbs []*pb.ProgressBar, logger *logrus.Log
 	}
 
 	// Otherwise fallback to fixed compact progress bars
-	if !stdoutTTY {
+	if !fl.stdoutTTY {
 		widthDelta = -pb.DefaultWidth
 		renderProgressBars = func(goBack bool) {
-			barText, _ := renderMultipleBars(stdoutTTY, goBack, maxLeft, termWidth, widthDelta, pbs, fl)
+			barText, _ := renderMultipleBars(fl.stdoutTTY, goBack, maxLeft, termWidth, widthDelta, pbs, fl)
 			progressBarsLastRenderLock.Lock()
 			progressBarsLastRender = []byte(barText)
 			progressBarsLastRenderLock.Unlock()
@@ -331,17 +331,17 @@ func showProgress(ctx context.Context, pbs []*pb.ProgressBar, logger *logrus.Log
 
 	// TODO: make configurable?
 	updateFreq := 1 * time.Second
-	if stdoutTTY {
+	if fl.stdoutTTY {
 		updateFreq = 100 * time.Millisecond
-		outMutex.Lock()
-		stdout.PersistentText = printProgressBars
-		stderr.PersistentText = printProgressBars
-		outMutex.Unlock()
+		fl.outMutex.Lock()
+		fl.stdout.PersistentText = printProgressBars
+		fl.stderr.PersistentText = printProgressBars
+		fl.outMutex.Unlock()
 		defer func() {
-			outMutex.Lock()
-			stdout.PersistentText = nil
-			stderr.PersistentText = nil
-			outMutex.Unlock()
+			fl.outMutex.Lock()
+			fl.stdout.PersistentText = nil
+			fl.stderr.PersistentText = nil
+			fl.outMutex.Unlock()
 		}()
 	}
 
@@ -361,12 +361,12 @@ func showProgress(ctx context.Context, pbs []*pb.ProgressBar, logger *logrus.Log
 		select {
 		case <-ctxDone:
 			renderProgressBars(false)
-			outMutex.Lock()
+			fl.outMutex.Lock()
 			printProgressBars()
-			outMutex.Unlock()
+			fl.outMutex.Unlock()
 			return
 		case <-winch:
-			if stdoutTTY && !errTermGetSize {
+			if fl.stdoutTTY && !errTermGetSize {
 				// More responsive progress bar resizing on platforms with SIGWINCH (*nix)
 				tw, _, err := term.GetSize(fd)
 				if tw > 0 && err == nil {
@@ -375,7 +375,7 @@ func showProgress(ctx context.Context, pbs []*pb.ProgressBar, logger *logrus.Log
 			}
 		case <-ticker.C:
 			// Default ticker-based progress bar resizing
-			if stdoutTTY && !errTermGetSize && winch == nil {
+			if fl.stdoutTTY && !errTermGetSize && winch == nil {
 				tw, _, err := term.GetSize(fd)
 				if tw > 0 && err == nil {
 					termWidth = tw
@@ -383,9 +383,9 @@ func showProgress(ctx context.Context, pbs []*pb.ProgressBar, logger *logrus.Log
 			}
 		}
 		renderProgressBars(true)
-		outMutex.Lock()
+		fl.outMutex.Lock()
 		printProgressBars()
-		outMutex.Unlock()
+		fl.outMutex.Unlock()
 	}
 }
 

APITeamLimited/globe-test

cmd/cloud.go
@@ -74,7 +74,7 @@ This will execute the test on the k6 cloud service. Use "k6 login cloud" to auth
 				***REMOVED***
 			***REMOVED***
 			// TODO: disable in quiet mode?
-			_, _ = fmt.Fprintf(stdout, "\n%s\n\n", getBanner(globalFlags.noColor || !stdoutTTY))
+			_, _ = fmt.Fprintf(globalFlags.stdout, "\n%s\n\n", getBanner(globalFlags.noColor || !globalFlags.stdoutTTY))
 
 			progressBar := pb.New(
 				pb.WithConstLeft("Init"),
@@ -230,7 +230,7 @@ This will execute the test on the k6 cloud service. Use "k6 login cloud" to auth
 			executionPlan := derivedConf.Scenarios.GetFullExecutionRequirements(et)
 			printExecutionDescription(
 				"cloud", filename, testURL, derivedConf, et,
-				executionPlan, nil, globalFlags.noColor || !stdoutTTY,
+				executionPlan, nil, globalFlags.noColor || !globalFlags.stdoutTTY, globalFlags,
 			)
 
 			modifyAndPrintBar(
@@ -320,8 +320,8 @@ This will execute the test on the k6 cloud service. Use "k6 login cloud" to auth
 				return errext.WithExitCodeIfNone(errors.New("Test progress error"), exitcodes.CloudFailedToGetProgress)
 			***REMOVED***
 
-			valueColor := getColor(globalFlags.noColor || !stdoutTTY, color.FgCyan)
-			fprintf(stdout, "     test status: %s\n", valueColor.Sprint(testProgress.RunStatusText))
+			valueColor := getColor(globalFlags.noColor || !globalFlags.stdoutTTY, color.FgCyan)
+			fprintf(globalFlags.stdout, "     test status: %s\n", valueColor.Sprint(testProgress.RunStatusText))
 
 			if testProgress.ResultStatus == cloudapi.ResultStatusFailed ***REMOVED***
 				// TODO: use different exit codes for failed thresholds vs failed test (e.g. aborted by system/limit)

cmd/pause.go
@@ -49,7 +49,7 @@ func getPauseCmd(ctx context.Context, globalFlags *commandFlags) *cobra.Command 
 			if err != nil ***REMOVED***
 				return err
 			***REMOVED***
-			return yamlPrint(stdout, status)
+			return yamlPrint(globalFlags.stdout, status)
 		***REMOVED***,
 	***REMOVED***
 	return pauseCmd

cmd/resume.go
@@ -50,7 +50,7 @@ func getResumeCmd(ctx context.Context, globalFlags *commandFlags) *cobra.Command
 				return err
 			***REMOVED***
 
-			return yamlPrint(stdout, status)
+			return yamlPrint(globalFlags.stdout, status)
 		***REMOVED***,
 	***REMOVED***
 	return resumeCmd

cmd/scale.go
@@ -54,7 +54,7 @@ func getScaleCmd(ctx context.Context, globalFlags *commandFlags) *cobra.Command 
 				return err
 			***REMOVED***
 
-			return yamlPrint(stdout, status)
+			return yamlPrint(globalFlags.stdout, status)
 		***REMOVED***,
 	***REMOVED***
 

cmd/stats.go
@@ -46,7 +46,7 @@ func getStatsCmd(ctx context.Context, globalFlags *commandFlags) *cobra.Command 
 				return err
 			***REMOVED***
 
-			return yamlPrint(stdout, metrics)
+			return yamlPrint(globalFlags.stdout, metrics)
 		***REMOVED***,
 	***REMOVED***
 	return statsCmd

cmd/status.go
@@ -46,7 +46,7 @@ func getStatusCmd(ctx context.Context, globalFlags *commandFlags) *cobra.Command
 				return err
 			***REMOVED***
 
-			return yamlPrint(stdout, status)
+			return yamlPrint(globalFlags.stdout, status)
 		***REMOVED***,
 	***REMOVED***
 	return statusCmd

cmd/ui.go
@@ -113,7 +113,7 @@ func printBar(bar *pb.ProgressBar, globalFlags *commandFlags) ***REMOVED***
 	// stateless... basically first render the left and right parts, so we know
 	// how long the longest line is, and how much space we have for the progress
 	widthDelta := -defaultTermWidth
-	if stdout.IsTTY ***REMOVED***
+	if globalFlags.stdout.IsTTY ***REMOVED***
 		// If we're in a TTY, instead of printing the bar and going to the next
 		// line, erase everything till the end of the line and return to the
 		// start, so that the next print will overwrite the same line.
@@ -124,7 +124,7 @@ func printBar(bar *pb.ProgressBar, globalFlags *commandFlags) ***REMOVED***
 	***REMOVED***
 	rendered := bar.Render(0, widthDelta)
 	// Only output the left and middle part of the progress bar
-	fprintf(stdout, "%s%s", rendered.String(), end)
+	fprintf(globalFlags.stdout, "%s%s", rendered.String(), end)
 ***REMOVED***
 
 func modifyAndPrintBar(bar *pb.ProgressBar, globalFlags *commandFlags, options ...pb.ProgressBarOption) ***REMOVED***
@@ -136,12 +136,12 @@ func modifyAndPrintBar(bar *pb.ProgressBar, globalFlags *commandFlags, options .
 // TODO: Clean this up as part of #1499 or #1427
 func printExecutionDescription(
 	execution, filename, outputOverride string, conf Config, et *lib.ExecutionTuple,
-	execPlan []lib.ExecutionStep, outputs []output.Output, noColor bool,
+	execPlan []lib.ExecutionStep, outputs []output.Output, noColor bool, globalFlags *commandFlags,
 ) ***REMOVED***
 	valueColor := getColor(noColor, color.FgCyan)
 
-	fprintf(stdout, "  execution: %s\n", valueColor.Sprint(execution))
-	fprintf(stdout, "     script: %s\n", valueColor.Sprint(filename))
+	fprintf(globalFlags.stdout, "  execution: %s\n", valueColor.Sprint(execution))
+	fprintf(globalFlags.stdout, "     script: %s\n", valueColor.Sprint(filename))
 
 	var outputDescriptions []string
 	switch ***REMOVED***
@@ -155,8 +155,8 @@ func printExecutionDescription(
 		***REMOVED***
 	***REMOVED***
 
-	fprintf(stdout, "     output: %s\n", valueColor.Sprint(strings.Join(outputDescriptions, ", ")))
-	fprintf(stdout, "\n")
+	fprintf(globalFlags.stdout, "     output: %s\n", valueColor.Sprint(strings.Join(outputDescriptions, ", ")))
+	fprintf(globalFlags.stdout, "\n")
 
 	maxDuration, _ := lib.GetEndOffset(execPlan)
 	executorConfigs := conf.Scenarios.GetSortedConfigs()
@@ -166,16 +166,16 @@ func printExecutionDescription(
 		scenarioDesc = fmt.Sprintf("%d scenarios", len(executorConfigs))
 	***REMOVED***
 
-	fprintf(stdout, "  scenarios: %s\n", valueColor.Sprintf(
+	fprintf(globalFlags.stdout, "  scenarios: %s\n", valueColor.Sprintf(
 		"(%.2f%%) %s, %d max VUs, %s max duration (incl. graceful stop):",
 		conf.ExecutionSegment.FloatLength()*100, scenarioDesc,
 		lib.GetMaxPossibleVUs(execPlan), maxDuration.Round(100*time.Millisecond)),
 	)
 	for _, ec := range executorConfigs ***REMOVED***
-		fprintf(stdout, "           * %s: %s\n",
+		fprintf(globalFlags.stdout, "           * %s: %s\n",
 			ec.GetName(), ec.GetDescription(et))
 	***REMOVED***
-	fprintf(stdout, "\n")
+	fprintf(globalFlags.stdout, "\n")
 ***REMOVED***
 
 //nolint: funlen
@@ -279,7 +279,7 @@ func showProgress(ctx context.Context, pbs []*pb.ProgressBar, logger *logrus.Log
 
 	var errTermGetSize bool
 	termWidth := defaultTermWidth
-	if stdoutTTY ***REMOVED***
+	if globalFlags.stdoutTTY ***REMOVED***
 		tw, _, err := term.GetSize(int(os.Stdout.Fd()))
 		if !(tw > 0) || err != nil ***REMOVED***
 			errTermGetSize = true
@@ -304,14 +304,15 @@ func showProgress(ctx context.Context, pbs []*pb.ProgressBar, logger *logrus.Log
 
 	printProgressBars := func() ***REMOVED***
 		progressBarsLastRenderLock.Lock()
-		_, _ = stdout.Writer.Write(progressBarsLastRender)
+		_, _ = globalFlags.stdout.Writer.Write(progressBarsLastRender)
 		progressBarsLastRenderLock.Unlock()
 	***REMOVED***
 
 	var widthDelta int
 	// Default to responsive progress bars when in an interactive terminal
 	renderProgressBars := func(goBack bool) ***REMOVED***
-		barText, longestLine := renderMultipleBars(stdoutTTY, goBack, maxLeft, termWidth, widthDelta, pbs, globalFlags)
+		barText, longestLine := renderMultipleBars(
+			globalFlags.stdoutTTY, goBack, maxLeft, termWidth, widthDelta, pbs, globalFlags)
 		widthDelta = termWidth - longestLine - termPadding
 		progressBarsLastRenderLock.Lock()
 		progressBarsLastRender = []byte(barText)
@@ -319,10 +320,10 @@ func showProgress(ctx context.Context, pbs []*pb.ProgressBar, logger *logrus.Log
 	***REMOVED***
 
 	// Otherwise fallback to fixed compact progress bars
-	if !stdoutTTY ***REMOVED***
+	if !globalFlags.stdoutTTY ***REMOVED***
 		widthDelta = -pb.DefaultWidth
 		renderProgressBars = func(goBack bool) ***REMOVED***
-			barText, _ := renderMultipleBars(stdoutTTY, goBack, maxLeft, termWidth, widthDelta, pbs, globalFlags)
+			barText, _ := renderMultipleBars(globalFlags.stdoutTTY, goBack, maxLeft, termWidth, widthDelta, pbs, globalFlags)
 			progressBarsLastRenderLock.Lock()
 			progressBarsLastRender = []byte(barText)
 			progressBarsLastRenderLock.Unlock()
@@ -331,17 +332,17 @@ func showProgress(ctx context.Context, pbs []*pb.ProgressBar, logger *logrus.Log
 
 	// TODO: make configurable?
 	updateFreq := 1 * time.Second
-	if stdoutTTY ***REMOVED***
+	if globalFlags.stdoutTTY ***REMOVED***
 		updateFreq = 100 * time.Millisecond
-		outMutex.Lock()
-		stdout.PersistentText = printProgressBars
-		stderr.PersistentText = printProgressBars
-		outMutex.Unlock()
+		globalFlags.outMutex.Lock()
+		globalFlags.stdout.PersistentText = printProgressBars
+		globalFlags.stderr.PersistentText = printProgressBars
+		globalFlags.outMutex.Unlock()
 		defer func() ***REMOVED***
-			outMutex.Lock()
-			stdout.PersistentText = nil
-			stderr.PersistentText = nil
-			outMutex.Unlock()
+			globalFlags.outMutex.Lock()
+			globalFlags.stdout.PersistentText = nil
+			globalFlags.stderr.PersistentText = nil
+			globalFlags.outMutex.Unlock()
 		***REMOVED***()
 	***REMOVED***
 
@@ -361,12 +362,12 @@ func showProgress(ctx context.Context, pbs []*pb.ProgressBar, logger *logrus.Log
 		select ***REMOVED***
 		case <-ctxDone:
 			renderProgressBars(false)
-			outMutex.Lock()
+			globalFlags.outMutex.Lock()
 			printProgressBars()
-			outMutex.Unlock()
+			globalFlags.outMutex.Unlock()
 			return
 		case <-winch:
-			if stdoutTTY && !errTermGetSize ***REMOVED***
+			if globalFlags.stdoutTTY && !errTermGetSize ***REMOVED***
 				// More responsive progress bar resizing on platforms with SIGWINCH (*nix)
 				tw, _, err := term.GetSize(fd)
 				if tw > 0 && err == nil ***REMOVED***
@@ -375,7 +376,7 @@ func showProgress(ctx context.Context, pbs []*pb.ProgressBar, logger *logrus.Log
 			***REMOVED***
 		case <-ticker.C:
 			// Default ticker-based progress bar resizing
-			if stdoutTTY && !errTermGetSize && winch == nil ***REMOVED***
+			if globalFlags.stdoutTTY && !errTermGetSize && winch == nil ***REMOVED***
 				tw, _, err := term.GetSize(fd)
 				if tw > 0 && err == nil ***REMOVED***
 					termWidth = tw
@@ -383,9 +384,9 @@ func showProgress(ctx context.Context, pbs []*pb.ProgressBar, logger *logrus.Log
 			***REMOVED***
 		***REMOVED***
 		renderProgressBars(true)
-		outMutex.Lock()
+		globalFlags.outMutex.Lock()
 		printProgressBars()
-		outMutex.Unlock()
+		globalFlags.outMutex.Unlock()
 	***REMOVED***
 ***REMOVED***
 