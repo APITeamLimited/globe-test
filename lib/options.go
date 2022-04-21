e9fcf72771917487a475f318b74ee4dbe21e622b
Gabriel Santos <gabrielopesantos97@gmail.com>
2022-04-21 15:57:54 +0100

    TLS Auth - Support for encrypted private key (#2488)

--- lib/options.go
+++ lib/options.go
@@ -22,7 +22,9 @@ package lib
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
+	"encoding/pem"
 	"fmt"
 	"net"
 	"reflect"
@@ -130,8 +132,9 @@ func (s *TLSCipherSuites) UnmarshalJSON(data []byte) error ***REMOVED***
 // Fields for TLSAuth. Unmarshalling hack.
 type TLSAuthFields struct ***REMOVED***
 	// Certificate and key as a PEM-encoded string, including "-----BEGIN CERTIFICATE-----".
-	Cert string `json:"cert"`
-	Key  string `json:"key"`
+	Cert     string `json:"cert"`
+	Key      string `json:"key"`
+	Password string `json:"password"`
 
 	// Domains to present the certificate to. May contain wildcards, eg. "*.example.com".
 	Domains []string `json:"domains"`
@@ -154,14 +157,50 @@ func (c *TLSAuth) UnmarshalJSON(data []byte) error ***REMOVED***
 ***REMOVED***
 
 func (c *TLSAuth) Certificate() (*tls.Certificate, error) ***REMOVED***
+	key := []byte(c.Key)
+	var err error
+	if c.Password != "" ***REMOVED***
+		key, err = decryptPrivateKey(c.Key, c.Password)
+		if err != nil ***REMOVED***
+			return nil, err
+		***REMOVED***
+	***REMOVED***
 	if c.certificate == nil ***REMOVED***
-		cert, err := tls.X509KeyPair([]byte(c.Cert), []byte(c.Key))
+		cert, err := tls.X509KeyPair([]byte(c.Cert), key)
 		if err != nil ***REMOVED***
 			return nil, err
 		***REMOVED***
 		c.certificate = &cert
 	***REMOVED***
 	return c.certificate, nil
+***REMOVED***
+
+func decryptPrivateKey(privKey, password string) ([]byte, error) ***REMOVED***
+	key := []byte(privKey)
+
+	block, _ := pem.Decode(key)
+	if block == nil ***REMOVED***
+		return nil, fmt.Errorf("failed to decode PEM key")
+	***REMOVED***
+
+	blockType := block.Type
+	if blockType == "ENCRYPTED PRIVATE KEY" ***REMOVED***
+		return nil, fmt.Errorf("encrypted pkcs8 formatted key is not supported")
+	***REMOVED***
+	/*
+	   Even though `DecryptPEMBlock` has been deprecated since 1.16.x it is still
+	   being used here because it is deprecated due to it not supporting *good* crypography
+	   ultimately though we want to support something so we will be using it for now.
+	*/
+	decryptedKey, err := x509.DecryptPEMBlock(block, []byte(password)) // nolint: staticcheck
+	if err != nil ***REMOVED***
+		return nil, err
+	***REMOVED***
+	key = pem.EncodeToMemory(&pem.Block***REMOVED***
+		Type:  blockType,
+		Bytes: decryptedKey,
+	***REMOVED***)
+	return key, nil
 ***REMOVED***
 
 // IPNet is a wrapper around net.IPNet for JSON unmarshalling
