e1623fd80e398f5d3eea1193228a063e910f6238
Harry Twigg <harrytwigg111@gmail.com>
2022-11-09 11:28:31 +0000

    Build updates

--- build-worker.sh
+++
@@ -1,4 +0,0 @@
-docker build -t worker-base:latest . -f worker/DockerfileBase
-docker build -t worker . -f worker/Dockerfile
-docker tag worker apiteamdevops/worker:latest
-docker push apiteamdevops/worker:latest
