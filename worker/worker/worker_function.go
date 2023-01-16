6ef78caa9e80c2dc2110912b9f60ea3aabc2fca9
Harry Twigg <harrytwigg111@gmail.com>
2023-01-16 17:41:13 +0000

    Force exit

--- worker/worker/worker_function.go
+++ worker/worker/worker_function.go
@@ -54,6 +54,9 @@ func RunWorkerFunction(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+
+	// Force the function to exit
+	os.Exit(0)
 }
 
 func RunDevWorkerServer() {
